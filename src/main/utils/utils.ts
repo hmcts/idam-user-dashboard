98fa8e9069759e9e1e16c6815e33341a60d60935
kiankwa <kian.kwa@justice.gov.uk>
2021-11-17 16:48:21 +0000

    Removed insecure temporary code for testing sonarqube code coverage

--- src/main/utils/utils.ts
+++ src/main/utils/utils.ts
@@ -1,14 +1,3 @@
 export const isObjectEmpty = (obj: {}): boolean => {
   return Object.keys(obj).length === 0;
 };
-
-export function validateUrlFormat(url: string): boolean {
-  const regexp = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '(([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}'+ // domain name and extension
-    '(:\\d+)?'+ // port
-    '(\\/[-a-z\\d%@_.~+&:]*)*'+ // path
-    '(\\?[;&a-z\\d%@_.,~+&:=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-
-  return regexp.test(url);
-}
