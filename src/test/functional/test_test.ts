ce0580f5ee88cce4367135367585ed28c3f4d2c4
Thomas-Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2021-11-22 14:46:50 +0000

    Switch to CodeceptJS (#14)
    * Added codeceptjs package and plugins playwright, allure
    
    * Config files for codeceptjs
    
    * Test tag configuration
    
    Co-authored-by: ashwini.venkatesha <ashwini.venkatesha@justice.gov.uk>

---
+++ src/test/functional/test_test.ts
@@ -0,0 +1,34 @@
+Feature('testing');
+
+Scenario('test1 @Smoke', ({ I }) => {
+  I.amOnPage('https://www.amazon.co.uk/');
+  I.resizeWindow(2200,1200);
+  I.click('#twotabsearchtextbox');
+  I.fillField('#twotabsearchtextbox', 'Books');
+  I.pressKey('Enter');
+});
+
+
+Scenario('test2 @Smoke', ({ I }) => {
+  I.amOnPage('https://www.amazon.co.uk/');
+  I.resizeWindow(2200,1200);
+  I.click('#twotabsearchtextbox');
+  I.fillField('#twotabsearchtextbox', 'Books');
+  I.pressKey('Enter');
+});
+
+Scenario('test3', ({ I }) => {
+  I.amOnPage('https://www.amazon.co.uk/');
+  I.resizeWindow(2200,1200);
+  I.click('#twotabsearchtextbox');
+  I.fillField('#twotabsearchtextbox', 'Books');
+  I.pressKey('Enter');
+});
+
+Scenario('test4', ({ I }) => {
+  I.amOnPage('https://www.amazon.co.uk/');
+  I.resizeWindow(2200,1200);
+  I.click('#twotabsearchtextbox');
+  I.fillField('#twotabsearchtextbox', 'Books');
+  I.pressKey('Enter');
+});
