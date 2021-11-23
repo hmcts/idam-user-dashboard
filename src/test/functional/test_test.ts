d917d5c3e97160f609bc7de22ac00063b5c247f7
kiankwa <kian.kwa@justice.gov.uk>
2021-11-23 17:00:51 +0000

    Test whether the test results can be shown in Blue Ocean

--- src/test/functional/test_test.ts
+++ src/test/functional/test_test.ts
@@ -1,6 +1,6 @@
 Feature('testing');
 
-Scenario('test1 @Smoke', ({ I }) => {
+Scenario('test1 @Nightly', ({ I }) => {
   I.amOnPage('https://www.amazon.co.uk/');
   I.resizeWindow(2200,1200);
   I.click('#twotabsearchtextbox');
@@ -9,7 +9,7 @@ Scenario('test1 @Smoke', ({ I }) => {
 });
 
 
-Scenario('test2 @Smoke', ({ I }) => {
+Scenario('test2 @Nightly', ({ I }) => {
   I.amOnPage('https://www.amazon.co.uk/');
   I.resizeWindow(2200,1200);
   I.click('#twotabsearchtextbox');
