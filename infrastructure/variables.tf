08f22cd72c6e35e29b2fdcda4a0a33ceea766b46
kiankwa <kian.kwa@justice.gov.uk>
2021-11-29 15:23:19 +0000

    First attempt to add infrastructure terraform files

---
+++ infrastructure/variables.tf
@@ -0,0 +1,23 @@
+// Infrastructural variables
+
+variable "product" {}
+
+variable "component" {}
+
+variable "location" {
+  default = "UK South"
+}
+
+variable "app" {
+  default = "user-dashboard"
+}
+
+variable "env" {}
+
+variable "subscription" {}
+
+variable "deployment_namespace" {}
+
+variable "common_tags" {
+  type = "map(string)"
+}
