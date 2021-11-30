2fd74e0e5f4fcd65c5c49eeedb2789f09dda8959
KianKwa <80688761+KianKwa@users.noreply.github.com>
2021-11-30 12:33:37 +0000

    SIDM-6645 First attempt to add infrastructure terraform files (#46)
    * First attempt to add infrastructure terraform files
    
    * UPdate terraform file to remove hardcoded idam value

---
+++ infrastructure/main.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  env  = "${var.env == "idam-preview" && var.product == "idam" ? "idam-dev" : var.env}"
+  tags = "${merge(var.common_tags, map("environment", local.env))}"
+}
+
+data "azurerm_virtual_network" "idam" {
+  name                = "core-infra-vnet-${var.env}"
+  resource_group_name = "core-infra-${var.env}"
+}
+
+data "azurerm_subnet" "redis" {
+  name                 = element(data.azurerm_virtual_network.idam.subnets, 3)
+  virtual_network_name = data.azurerm_virtual_network.idam.name
+  resource_group_name  = "core-infra-${var.env}"
+}
+
+data "azurerm_key_vault" "idam" {
+  name                = local.vault_name
+  resource_group_name = "${var.product}-${var.env}"
+}
