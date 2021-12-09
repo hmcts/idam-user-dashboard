df86189341d085a531fc81c1eb12e62c380fe761
ashwini.venkatesha <ashwini.venkatesha@justice.gov.uk>
2021-12-09 13:43:52 +0000

    pally-initial-commit

--- jest.a11y.config.js
+++ jest.a11y.config.js
@@ -10,4 +10,13 @@ module.exports = {
     '^.+\\.ts?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+"reporters": [
+    "default",
+    ["./test-output/pa11y/reports", {
+      "pageTitle": "Accessibility Test Report",
+      "outputPath": "./test-output/functional/test-report.html",
+      "includeFailureMsg": true
+    }]
+  ]
 }
+
