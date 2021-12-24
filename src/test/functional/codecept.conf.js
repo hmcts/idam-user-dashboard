84021e85dc87d47b5c49c0b17f79f319136a2484
Thomas Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2021-12-24 15:59:30 +0000

    SIDM-6801 - Fix for issue with file not registering as module with tsconfig

--- src/test/functional/codecept.conf.js
+++ src/test/functional/codecept.conf.js
@@ -1,36 +1,3 @@
-require('ts-node').register({project: 'src/test/tsconfig.json'});
-import {config} from '../config';
+require('ts-node').register({ project: 'src/test/tsconfig.json' });
 
-
-exports.config = {
-  tests: './*-test.ts',
-  output: '../../../test-output/functional/reports',
-  helpers: {
-    Playwright: {
-      url: config.TEST_URL,
-      show: false,
-      browser: 'chromium',
-    },
-  },
-  include: {
-    I: './steps_file.js',
-  },
-  bootstrap: null,
-  mocha: {},
-  name: 'functional',
-  plugins: {
-    pauseOnFail: {},
-    retryFailedStep: {
-      enabled: true,
-    },
-    allure: {
-      enabled: true,
-    },
-    tryTo: {
-      enabled: true,
-    },
-    screenshotOnFail: {
-      enabled: true,
-    },
-  },
-};
+module.exports = require('./codecept.conf.ts');
