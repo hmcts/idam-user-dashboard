1e8fbc27d509cc083710554c02df1a1c8e17b934
ashwini-mv <84072035+ashwini-mv@users.noreply.github.com>
2022-01-04 22:35:25 +0000

    Added login functionality to accessibility test and changed codecept.… (#84)
    * Added login functionality to accessibility test and changed codecept.conf.js and saucelabs.conf.js  to use local URL and removed amazon URL from sample tests
    
    * Update Pa11y test to use smoke test username and password from environment variables
    
    * Added login functionality to accessibility test and changed codecept.conf.js and saucelabs.conf.js  to use local URL and removed amazon URL from sample tests
    
    * Configured username and password to come from test-data
    
    * Fixed the requested changes
    
    * Added login functionality to accessibility test and changed codecept.conf.js and saucelabs.conf.js  to use local URL and removed amazon URL from sample tests
    
    * Added login functionality to accessibility test and changed codecept.conf.js and saucelabs.conf.js  to use local URL and removed amazon URL from sample tests
    
    * SIDM-6801 - Fix for issue with file not registering as module with tsconfig
    
    * SIDM-6801 - Forgot to include changes in previous commit
    
    * SIDM-6801 - Ignore HTTPS errors
    
    * SIDM-6801 - Cleaned up duplicate playwright helper code
    
    * Added login functionality to accessibility test and changed codecept.conf.js and saucelabs.conf.js  to use local URL and removed amazon URL from sample tests
    
    * Increased the codeceptJs and Playwright timeout limit
    
    * Replaced assertion from text to url
    
    * Reverted back assertion from url to text
    
    * Changed assertion text to rule out '?' symbol problem
    
    * Increasing the wait for time out to max
    
    * SIDM-6801 - Fix for cross-browser tests
    
    * Set wait-for-time-out to 2 sec and added 2 more accessibility URLs
    
    * idam-config - Configured idam-pr dependency and environment specific callback URL
    
    Co-authored-by: kiankwa <kian.kwa@justice.gov.uk>
    Co-authored-by: Thomas Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>

--- src/test/cross-browser/test_test.ts
+++ src/test/cross-browser/test_test.ts
@@ -1,34 +1,13 @@
 Feature('testing');
 
-Scenario('test1', ({ I }) => {
-  I.amOnPage('https://www.amazon.co.uk/');
-  I.resizeWindow(2200,1200);
-  I.click('#twotabsearchtextbox');
-  I.fillField('#twotabsearchtextbox', 'Books');
-  I.pressKey('Enter');
+Scenario('test1', ({I}) => {
+  I.amOnPage('/login');
+  I.resizeWindow(2200, 1200);
 });
 
 
-Scenario('test2', ({ I }) => {
-  I.amOnPage('https://www.amazon.co.uk/');
-  I.resizeWindow(2200,1200);
-  I.click('#twotabsearchtextbox');
-  I.fillField('#twotabsearchtextbox', 'Books');
-  I.pressKey('Enter');
+Scenario('test2', ({I}) => {
+  I.amOnPage('/login');
+  I.resizeWindow(2200, 1200);
 });
 
-Scenario('test3', ({ I }) => {
-  I.amOnPage('https://www.amazon.co.uk/');
-  I.resizeWindow(2200,1200);
-  I.click('#twotabsearchtextbox');
-  I.fillField('#twotabsearchtextbox', 'Books');
-  I.pressKey('Enter');
-});
-
-Scenario('test4', ({ I }) => {
-  I.amOnPage('https://www.amazon.co.uk/');
-  I.resizeWindow(2200,1200);
-  I.click('#twotabsearchtextbox');
-  I.fillField('#twotabsearchtextbox', 'Books');
-  I.pressKey('Enter');
-});
