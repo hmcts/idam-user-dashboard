ca0039c7da071dc1c8745626b682278e8cce0d3c
ashwini.venkatesha <ashwini.venkatesha@justice.gov.uk>
2022-01-13 13:25:42 +0000

    Using custom-helper and custom-class actor

--- src/test/functional/steps.d.ts
+++ src/test/functional/steps.d.ts
@@ -1,10 +1,22 @@
+// /// <reference types='codeceptjs' />
+// type steps_file = typeof import('./steps_file.js');
+//
+// declare namespace CodeceptJS {
+//   interface SupportObject { I: I, current: any }
+//   interface Methods extends Playwright {}
+//   //interface I extends ReturnType<steps_file> {}
+//   interface I extends WithTranslation<Methods> {}
+//   namespace Translation {
+//     interface Actions {}
+//   }
+// }
 
-
-type stepsFile = typeof import('./steps_file');
+/// <reference types='codeceptjs' />
+type customMethods = typeof import('./steps_file');
 type CustomHelper = import('./shared/idam-helper');
 
 declare namespace CodeceptJS {
-  interface SupportObject { I: I, stepsFile: stepsFile}
+  interface SupportObject { I: I, customMethods: customMethods}
   interface Methods extends Playwright, CustomHelper {}
   interface I extends WithTranslation<Methods> {}
   namespace Translation {
