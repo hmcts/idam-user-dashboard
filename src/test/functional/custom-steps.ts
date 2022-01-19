bc7caef8a56eb4aecf78ef252fbf9b937319b545
ashwini.venkatesha <ashwini.venkatesha@justice.gov.uk>
2022-01-19 16:01:27 +0000

    Added user-details

--- src/test/functional/custom-steps.ts
+++ src/test/functional/custom-steps.ts
@@ -12,7 +12,6 @@ export = function () {
       this.see('Sign in');
       this.fillField('#username', username);
       this.fillField('#password', password);
-      this.wait(5);
       this.click('Sign in');
       this.waitForText('Please select an option to continue');
     },
--- src/test/functional/search-user-test.ts
+++ src/test/functional/search-user-test.ts
@@ -1,5 +1,6 @@
 import {config} from '../config';
 import StringOrSecret = CodeceptJS.StringOrSecret;
+import * as Assert from 'assert';
 
 Feature('Search User');
 const incorrectEmailAddresses = new DataTable(['incorrectEmailAddress']);
@@ -16,19 +17,30 @@ Before(({I}) => {
   I.waitForText('Please enter the email address of the user you wish to manage');
 });
 
-Data(incorrectEmailAddresses).Scenario('I as an user should be able to see proper error message if search text is not in the right format', ({I, current}) => {
+Data(incorrectEmailAddresses).Scenario('I as an user should be able to see proper error message if search text is not in the right format', ({I,current}) => {
   I.click('#email');
   I.fillField('#email', current.incorrectEmailAddress);
   I.click('Search');
   I.seeElement('#email-error');
   I.waitForText('The email address is not in the correct format');
-});
+}).retry(config.SCENARIO_RETRY_LIMIT);
 
-Scenario('I as an user should be able to Search for users', ({I}) => {
+Scenario('I as an user should be able to Search for users', async ({I}) => {
   I.click('#email');
   I.fillField('#email', config.SMOKE_TEST_USER_USERNAME as StringOrSecret);
   I.click('Search');
   I.waitForText('User Details');
+  console.log('ashwini');
+  const status = await I.grabTextFrom('#status');
+  Assert.equal(status.trim(),'Active');
+  const email = await I.grabTextFrom('#email');
+  Assert.equal(email.trim(),'idamOwner@HMCTS.NET');
+  const firstName = await I.grabTextFrom('#first-name');
+  Assert.equal(firstName.trim(),'System');
+  const lastName = await I.grabTextFrom('#last-name');
+  Assert.equal(lastName.trim(),'Owner');
+  const assignedRoles = await I.grabTextFrom('#assigned-roles');
+  Assert.equal(assignedRoles.trim(),'IDAM_SYSTEM_OWNER');
 }).retry(config.SCENARIO_RETRY_LIMIT);
 
 Scenario('I as an user should be able to see proper error message if search text left blank', ({I}) => {
