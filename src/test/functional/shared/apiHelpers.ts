2aeb82089fe75f5a1f399e3265384aa3a1fd22a7
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-02-01 10:33:46 +0000

    SIDM-6943 - Removed old/unused API calls

--- src/test/functional/shared/apiHelpers.ts
+++ src/test/functional/shared/apiHelpers.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import config from 'config';
 import { config as testConfig } from '../../config';
 
-export const createUserWithRoles = async(email, forename, userRoles) => {
+export const createUserWithRoles = async(email, password, forename, userRoles) => {
   const codeUserRoles = userRoles.map(role => ({ code: role }));
 
   try {
@@ -10,7 +10,7 @@ export const createUserWithRoles = async(email, forename, userRoles) => {
       `${config.get('services.idam.url.api')}/testing-support/accounts`,
       {
         email: email,
-        password: testConfig.PASSWORD,
+        password: password,
         forename: forename,
         surname: testConfig.SUPER_ADMIN_CITIZEN_USER_LASTNAME,
         roles: codeUserRoles
@@ -21,39 +21,6 @@ export const createUserWithRoles = async(email, forename, userRoles) => {
 
   } catch (e) {
     throw new Error(`Failed to create user ${email} with roles ${userRoles}, http-status: ${e.response.status}`);
-  }
-};
-
-export const getAuthToken = async() => {
-  const credentials = {
-    username: testConfig.SMOKE_TEST_USER_USERNAME as string,
-    password: testConfig.SMOKE_TEST_USER_PASSWORD as string
-  };
-
-  try {
-    const response = await axios.post(
-      `${config.get('services.idam.url.api')}/loginUser`,
-      new URLSearchParams(credentials)
-    );
-
-    return response.data.api_auth_token;
-  } catch (e) {
-    throw new Error(`Failed to get admin auth-token with ${credentials.username}:${credentials.password}, http-status: ${e.response.status}`);
-  }
-};
-
-export const retireStaleUser = async(userId) => {
-  const authToken = await getAuthToken();
-  try {
-    await axios.post(
-      `${config.get('services.idam.url.api')}/api/v1/staleUsers/${userId}/retire`,
-      {},
-      {
-        headers: {'Authorization': 'AdminApiAuthToken ' + authToken},
-      }
-    );
-  } catch (e) {
-    throw new Error(`Failed to retire: ${userId}, http-status: ${e.response.status}`);
   }
 };
 
