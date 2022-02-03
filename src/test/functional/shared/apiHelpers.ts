47cb60cae1e1e177cf1e90a27ae84ce7975d1fa1
Thomas-Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-02-03 10:21:40 +0000

    Re-added removed API calls

--- src/test/functional/shared/apiHelpers.ts
+++ src/test/functional/shared/apiHelpers.ts
@@ -6,7 +6,7 @@ export const createUserWithRoles = async(email, password, forename, userRoles) =
   const codeUserRoles = userRoles.map(role => ({ code: role }));
 
   try {
-    await axios.post(
+    return await axios.post(
       `${config.get('services.idam.url.api')}/testing-support/accounts`,
       {
         email: email,
@@ -17,12 +17,44 @@ export const createUserWithRoles = async(email, password, forename, userRoles) =
       },
       {
         headers: {'Content-Type': 'application/json'},
-      });
+      }).data;
 
   } catch (e) {
-    throw new Error(`Failed to create user ${email} with roles ${userRoles}, http-status: ${e.response.status}`);
+    throw new Error(`Failed to create user ${email} with roles ${userRoles}, http-status: ${e.response?.status}`);
   }
 };
+
+export const getAuthToken = async() => {
+   const credentials = {
+     username: testConfig.SMOKE_TEST_USER_USERNAME as string,
+     password: testConfig.SMOKE_TEST_USER_PASSWORD as string
+   };
+
+   try {
+     return await axios.post(
+       `${config.get('services.idam.url.api')}/loginUser`,
+       new URLSearchParams(credentials)
+     ).data.api_auth_token;
+     
+   } catch (e) {
+     throw new Error(`Failed to get admin auth-token with ${credentials.username}:${credentials.password}, http-status: ${e.response?.status}`);
+   }
+ };
+
+ export const retireStaleUser = async(userId) => {
+   const authToken = await getAuthToken();
+   try {
+     await axios.post(
+       `${config.get('services.idam.url.api')}/api/v1/staleUsers/${userId}/retire`,
+       {},
+       {
+         headers: {'Authorization': 'AdminApiAuthToken ' + authToken},
+       }
+     );
+   } catch (e) {
+     throw new Error(`Failed to retire: ${userId}, http-status: ${e.response?.status}`);
+   }
+ };
 
 export const deleteUser = async(email) => {
   try {
@@ -30,6 +62,6 @@ export const deleteUser = async(email) => {
       `${config.get('services.idam.url.api')}/testing-support/accounts/${email}`
     );
   } catch (e) {
-    throw new Error(`Failed to delete user with email ${email}, http-status: ${e.response.status}`);
+    throw new Error(`Failed to delete user with email ${email}, http-status: ${e.response?.status}`);
   }
 };
