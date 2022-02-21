52b705905b47940c5b434490a24be6d9eaefde52
ashwini.venkatesha <ashwini.venkatesha@justice.gov.uk>
2022-02-21 11:13:21 +0000

    Functional tests for search by sso-id and user-id

--- src/test/functional/search-user-test.ts
+++ src/test/functional/search-user-test.ts
@@ -1,15 +1,17 @@
 import { config as testConfig} from '../config';
 import * as Assert from 'assert';
 import {randomData} from './shared/random-data';
-import {createUserWithRoles} from './shared/apiHelpers';
+import {createUserWithRoles, createUserWithSsoId} from './shared/apiHelpers';
 
 Feature('Search User');
 
 const dashboardUserEMAIL= testConfig.TEST_SUITE_PREFIX + randomData.getRandomEmailAddress();
 const citizenUserEmail= testConfig.TEST_SUITE_PREFIX + randomData.getRandomEmailAddress();
+let citizenUser;
+
 BeforeSuite(async () => {
-  await createUserWithRoles(dashboardUserEMAIL, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, []);
-  await createUserWithRoles(citizenUserEmail, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, [testConfig.USER_ROLE_CITIZEN]);
+  createUserWithRoles(dashboardUserEMAIL, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, []);
+  citizenUser = await createUserWithSsoId(citizenUserEmail, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, [testConfig.USER_ROLE_CITIZEN], randomData.getRandomString(5));
 });
 
 const incorrectEmailAddresses = new DataTable(['incorrectEmailAddress']);
@@ -31,7 +33,7 @@ Data(incorrectEmailAddresses).Scenario('I as an user should be able to see prope
   I.waitForText('The email address is not in the correct format');
 });
 
-Scenario('@CrossBrowser I should be able to search for users', async ({I}) => {
+Scenario('@CrossBrowser I should be able to search with user-email', async ({I}) => {
   I.loginAs(dashboardUserEMAIL, testConfig.PASSWORD);
   I.waitForText('Add new users');
   I.waitForText('Manage existing users');
@@ -40,6 +42,62 @@ Scenario('@CrossBrowser I should be able to search for users', async ({I}) => {
   I.waitForText('Please enter the email address, user ID or SSO ID of the user you wish to manage');
   I.click('#search');
   I.fillField('#search', citizenUserEmail);
+  I.click('Search');
+  I.waitForText('User Details');
+
+  const status = await I.grabTextFrom('#status');
+  Assert.equal(status.trim(),'Active');
+
+  const email = await I.grabTextFrom('#email');
+  Assert.equal(email.trim(), citizenUserEmail);
+
+  const firstName = await I.grabTextFrom('#first-name');
+  Assert.equal(firstName.trim(), testConfig.USER_FIRSTNAME);
+
+  const lastName = await I.grabTextFrom('#last-name');
+  Assert.equal(lastName.trim(), testConfig.USER_LASTNAME);
+
+  const assignedRoles = await I.grabTextFrom('#assigned-roles');
+  Assert.equal(assignedRoles.trim(), testConfig.USER_ROLE_CITIZEN);
+});
+
+Scenario('@CrossBrowser I should be able to search with user-id', async ({I}) => {
+  I.loginAs(dashboardUserEMAIL, testConfig.PASSWORD);
+  I.waitForText('Add new users');
+  I.waitForText('Manage existing users');
+  I.click('Manage existing users');
+  I.click('Continue');
+  I.waitForText('Please enter the email address, user ID or SSO ID of the user you wish to manage');
+  I.click('#search');
+  I.fillField('#search', citizenUser.id);
+  I.click('Search');
+  I.waitForText('User Details');
+
+  const status = await I.grabTextFrom('#status');
+  Assert.equal(status.trim(),'Active');
+
+  const email = await I.grabTextFrom('#email');
+  Assert.equal(email.trim(), citizenUserEmail);
+
+  const firstName = await I.grabTextFrom('#first-name');
+  Assert.equal(firstName.trim(), testConfig.USER_FIRSTNAME);
+
+  const lastName = await I.grabTextFrom('#last-name');
+  Assert.equal(lastName.trim(), testConfig.USER_LASTNAME);
+
+  const assignedRoles = await I.grabTextFrom('#assigned-roles');
+  Assert.equal(assignedRoles.trim(), testConfig.USER_ROLE_CITIZEN);
+});
+
+Scenario('@CrossBrowser I should be able to search with sso-id', async ({I}) => {
+  I.loginAs(dashboardUserEMAIL, testConfig.PASSWORD);
+  I.waitForText('Add new users');
+  I.waitForText('Manage existing users');
+  I.click('Manage existing users');
+  I.click('Continue');
+  I.waitForText('Please enter the email address, user ID or SSO ID of the user you wish to manage');
+  I.click('#search');
+  I.fillField('#search', citizenUser.ssoId);
   I.click('Search');
   I.waitForText('User Details');
 
