e00fb453254109cd0dead6c3d9516b0967f4c1e2
kiankwa <kian.kwa@justice.gov.uk>
2022-02-25 09:31:01 +0000

    Update 'Add Users' screen with form to add new user's email

---
+++ src/main/controllers/AddUserDetailsController.ts
@@ -0,0 +1,45 @@
+import { AuthedRequest } from '../interfaces/AuthedRequest';
+import { Response } from 'express';
+import { RootController } from './RootController';
+import autobind from 'autobind-decorator';
+import { isEmpty, isValidEmailFormat } from '../utils/utils';
+import {
+  duplicatedEmailError,
+  INVALID_EMAIL_FORMAT_ERROR,
+  MISSING_EMAIL_ERROR
+} from '../utils/error';
+import { SearchType } from '../utils/SearchType';
+import asyncError from '../modules/error-handler/asyncErrorDecorator';
+
+@autobind
+export class AddUserDetailsController extends RootController{
+  public get(req: AuthedRequest, res: Response) {
+    return super.get(req, res, 'add-user-details');
+  }
+
+  @asyncError
+  public async post(req: AuthedRequest, res: Response) {
+      return await this.processNewUserEmail(req, res);
+  }
+
+  private async processNewUserEmail(req: AuthedRequest, res: Response) {
+    const email = req.body.email as string;
+    if (isEmpty(email)) {
+      return this.postError(req, res, MISSING_EMAIL_ERROR);
+    } else if (!isValidEmailFormat(email)) {
+      return this.postError(req, res, INVALID_EMAIL_FORMAT_ERROR);
+    }
+
+    // check if the user with the same email already exists
+    const users = await req.scope.cradle.api.getUserDetails(SearchType['Email'], email);
+    return users.length == 0
+      ? super.post(req, res, 'add-user-details', {content: {email}})
+      : this.postError(req, res, duplicatedEmailError(email));
+  }
+
+  private postError(req: AuthedRequest, res: Response, errorMessage: string) {
+    return super.post(req, res, 'add-users', { error: {
+        email: { message: errorMessage }
+    }});
+  }
+}
--- src/main/utils/urls.ts
+++ src/main/utils/urls.ts
@@ -6,6 +6,7 @@ export const OAUTH2_CALLBACK_URL = '/oauth2/callback';
 
 export const MANAGER_USERS_URL = '/users/manage';
 export const ADD_USERS_URL = '/users/add';
+export const ADD_USER_DETAILS_URL = '/users/add/details';
 export const USER_DETAILS_URL = '/users/details';
 export const EDIT_USER_URL = '/users/edit';
 
