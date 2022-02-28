3216f4bfe3d6d1bebc0f74348718fddbd74b39fa
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-02-28 11:07:01 +0000

    SIDM-7200 - Cleaned up test skipping more

--- src/test/functional/shared/featureFlags.ts
+++ src/test/functional/shared/featureFlags.ts
@@ -6,22 +6,19 @@ const colors = require('chalk');
 
 module.exports = function() {
   let flagValues: { [key: string]: boolean };
-  let launchDarkly: LaunchDarkly;
 
   event.dispatcher.on(event.all.before, async function () {
-    launchDarkly = new LaunchDarkly('idam-user-dashboard', process.env.LAUNCHDARKLY_SDK_KEY);
+    const launchDarkly = new LaunchDarkly('idam-user-dashboard', process.env.LAUNCHDARKLY_SDK_KEY);
     flagValues = await new FeatureFlags(launchDarkly).getAllFlagValues();
     launchDarkly.closeConnection();
   });
 
   event.dispatcher.on(event.test.before, function (test) {
-    if(test.opts && !isArrayEmpty(test.opts.featureFlags) && !isObjectEmpty(flagValues)) {
-      if(!test.opts.featureFlags.every(flag => flagValues[flag])) {
-        test.run = function skip() {
-          console.warn(`[00]   ${colors.yellow.bold('S')} ${test.title}`);
-          this.skip();
-        };
-      }
+    if(!isArrayEmpty(test.opts?.featureFlags) && !isObjectEmpty(flagValues) && !test.opts.featureFlags.every(flag => flagValues[flag])) {
+      test.run = function skip() {
+        console.warn(`[00]   ${colors.yellow.bold('S')} ${test.title}`);
+        this.skip();
+      };
     }
   });
 };
