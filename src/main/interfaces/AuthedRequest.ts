8c172bdfcb0167dca81203a6a70d0c072617f2fa
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-03-02 14:04:53 +0000

    SIDM-6606 - Updated User type and AuthedRequest type

--- src/main/interfaces/AuthedRequest.ts
+++ src/main/interfaces/AuthedRequest.ts
@@ -1,8 +1,12 @@
 import { Request } from 'express';
 import { IdamAPI } from '../app/idam-api/IdamAPI';
 import { AwilixContainer } from 'awilix';
+import { User } from './User';
 
 interface Auth {
+  session:
+    Express.Session &
+    { user: Partial<User> & { accessToken: string; idToken: string } };
   scope: AwilixContainer<{ api: IdamAPI }>;
 }
 
