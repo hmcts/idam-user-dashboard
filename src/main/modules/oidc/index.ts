45c7ae29ab81284164c202c3a88c94d089144341
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-03-09 23:32:48 +0000

    SIDM-6606 - Fix merge issue

--- src/main/modules/oidc/index.ts
+++ src/main/modules/oidc/index.ts
@@ -41,13 +41,13 @@ export class OidcMiddleware {
 
     // Reject any logged out, expired or bad sessions
     app.use((req: AuthedRequest, res: Response, next: NextFunction) => {
-      const { user, tokens } = req.session;
+      const {user, tokens} = req.session;
 
       if (!user) {
         return res.redirect(LOGIN_URL);
       }
 
-      if(OIDCToken.isExpired(tokens.accessToken)) {
+      if (OIDCToken.isExpired(tokens.accessToken)) {
         return req.session.destroy(() => {
           res.clearCookie(config.get('session.cookie.name'));
           res.redirect(LOGIN_URL);
@@ -95,6 +95,7 @@ export class OidcMiddleware {
 
       return next();
     });
+  }
 
   private saveConfiguredSession(newSession: OIDCSession, session: Partial<AppSession>): Promise<AppSession> {
     return new Promise((resolve) => {
