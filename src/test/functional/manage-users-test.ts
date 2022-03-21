c4f7a29ff9f3871a2bc4c3eae1f040909e52443e
ashwini-mv <84072035+ashwini-mv@users.noreply.github.com>
2022-03-21 15:48:46 +0000

    SIDM-7199 (#251)
    * Modified tests to see edit-user and suspend-user button visibility
    
    * Edit user validation conditions added for edit user, delete user and suspend user.
    
    Co-authored-by: Thomas-Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>

--- src/test/functional/manage-users-test.ts
+++ src/test/functional/manage-users-test.ts
@@ -1,4 +1,6 @@
 import {
+  assignRolesToParentRole,
+  createAssignableRoles,
   createUserWithRoles
 } from './shared/testingSupportApi';
 
@@ -10,14 +12,22 @@ import * as Assert from 'assert';
 import {randomData} from './shared/random-data';
 import {convertISODateTimeToUTCFormat} from '../../main/utils/utils';
 
+
+const PARENT_ROLE = randomData.getRandomRole();
+const ASSIGNABLE_CHILD_ROLE = randomData.getRandomRole();
 const dashboardUserEMAIL = randomData.getRandomEmailAddress();
+
 BeforeSuite(async () => {
-  await createUserWithRoles(dashboardUserEMAIL, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, [testConfig.RBAC.access]);
+  await createAssignableRoles(PARENT_ROLE);
+  await createAssignableRoles(ASSIGNABLE_CHILD_ROLE);
+  // Assigning self role with the child role so the this user can also delete same level users
+  await assignRolesToParentRole(PARENT_ROLE, [ASSIGNABLE_CHILD_ROLE, PARENT_ROLE]);
+  await createUserWithRoles(dashboardUserEMAIL, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, [testConfig.RBAC.access, PARENT_ROLE]);
 });
 
 Scenario('I as a user should be able to see the active status of a user', async ({I}) => {
   const activeUserEmail = randomData.getRandomEmailAddress();
-  await I.createUserWithSsoId(activeUserEmail, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, [testConfig.USER_ROLE_CITIZEN], randomData.getRandomSSOId());
+  await I.createUserWithSsoId(activeUserEmail, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, [ASSIGNABLE_CHILD_ROLE], randomData.getRandomSSOId());
   const activeUser = await I.getUserDetails(activeUserEmail);
 
   I.loginAs(dashboardUserEMAIL, testConfig.PASSWORD);
@@ -29,6 +39,9 @@ Scenario('I as a user should be able to see the active status of a user', async 
   I.fillField('#search', activeUserEmail);
   I.click('Search');
   I.waitForText('User Details');
+  I.see('Edit user');
+  I.see('Delete user');
+  I.see('Suspend user');
 
   const createDate = convertISODateTimeToUTCFormat(activeUser[0].createDate);
   const lastModified = convertISODateTimeToUTCFormat(activeUser[0].lastModified);
@@ -57,7 +70,7 @@ Scenario('I as a user should be able to see the active status of a user', async 
 
 Scenario('I as a user should be able to see the suspended status of a user', async ({I}) => {
   const suspendUserEmail = randomData.getRandomEmailAddress();
-  const user = await I.createUserWithRoles(suspendUserEmail, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, [testConfig.USER_ROLE_CITIZEN]);
+  const user = await I.createUserWithRoles(suspendUserEmail, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, [ASSIGNABLE_CHILD_ROLE]);
   await I.suspendUser(user.id, suspendUserEmail);
 
   I.loginAs(dashboardUserEMAIL, testConfig.PASSWORD);
@@ -69,6 +82,9 @@ Scenario('I as a user should be able to see the suspended status of a user', asy
   I.fillField('#search', suspendUserEmail);
   I.click('Search');
   I.waitForText('User Details');
+  I.see('Edit user');
+  I.see('Delete user');
+  I.see('Un-suspend user');
 
   const email = await I.grabTextFrom('#email');
   Assert.equal(email.trim(), suspendUserEmail);
@@ -79,7 +95,7 @@ Scenario('I as a user should be able to see the suspended status of a user', asy
 
 Scenario('I as a user should be able to see the stale status of a user', async ({I}) => {
   const staleUserEmail = randomData.getRandomEmailAddress();
-  const user = await I.createUserWithRoles(staleUserEmail, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, [testConfig.USER_ROLE_CITIZEN]);
+  const user = await I.createUserWithRoles(staleUserEmail, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, [ASSIGNABLE_CHILD_ROLE]);
   await I.retireStaleUser(user.id);
 
   await I.loginAs(dashboardUserEMAIL, testConfig.PASSWORD);
@@ -91,6 +107,9 @@ Scenario('I as a user should be able to see the stale status of a user', async (
   I.fillField('#search', staleUserEmail);
   I.click('Search');
   I.waitForText('User Details');
+  I.dontSee('Edit user');
+  I.dontSee('Delete user');
+  I.dontSee('Suspend user');
 
   const email = await I.grabTextFrom('#email');
   Assert.equal(email.trim(), staleUserEmail);
