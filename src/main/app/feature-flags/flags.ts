b8cce2f2b946484c4a294fb90ccadc3895fde2e2
Thomas-Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-03-24 09:55:44 +0000

    SIDM-7290 - Wrapped each beta feature in its own flag (#265)

--- src/main/app/feature-flags/flags.ts
+++ src/main/app/feature-flags/flags.ts
@@ -1,1 +1,4 @@
-export const BETA_FEATURES = 'idam-user-dashboard--beta-features';
+export const BETA_ADD = 'idam-user-dashboard--beta-add';
+export const BETA_EDIT = 'idam-user-dashboard--beta-edit';
+export const BETA_SUSPEND = 'idam-user-dashboard--beta-suspend';
+export const BETA_DELETE = 'idam-user-dashboard--beta-delete';
