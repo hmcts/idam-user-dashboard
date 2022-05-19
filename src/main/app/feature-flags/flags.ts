a3bf19b9ce2b09142772f27b4329ec25f1a293cc
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-05-19 15:14:30 +0100

    feature-flags - Wrapped MFA feature with gamma flag

--- src/main/app/feature-flags/flags.ts
+++ src/main/app/feature-flags/flags.ts
@@ -2,3 +2,4 @@ export const BETA_ADD = 'idam-user-dashboard--beta-add';
 export const BETA_EDIT = 'idam-user-dashboard--beta-edit';
 export const BETA_SUSPEND = 'idam-user-dashboard--beta-suspend';
 export const BETA_DELETE = 'idam-user-dashboard--beta-delete';
+export const GAMMA_MFA = 'idam-user-dashboard--gamma-mfa';
