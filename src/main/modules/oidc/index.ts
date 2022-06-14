0dfae95585220cd1b7d0b80633b2a591caecaf1a
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-06-14 10:16:08 +0100

    SIDM-7454 - Updated OIDC implementation, reintroduced caching system user token

--- src/main/modules/oidc/index.ts
+++ src/main/modules/oidc/index.ts
@@ -1,6 +1,21 @@
-import { Application } from 'express';
+import { Application, NextFunction, Request, Response } from 'express';
 import config from 'config';
-const { auth } = require('express-openid-connect');
+import { AuthedRequest } from '../../interfaces/AuthedRequest';
+import { asClass, asValue } from 'awilix';
+import { IdamAPI } from '../../app/idam-api/IdamAPI';
+import axios, { AxiosInstance } from 'axios';
+import jwtDecode from 'jwt-decode';
+import { HTTPError } from '../../app/errors/HttpError';
+import { constants as http } from 'http2';
+import { Session } from 'express-openid-connect';
+import ConnectRedis from 'connect-redis';
+import session from 'express-session';
+import FileStoreFactory from 'session-file-store';
+import { createClient } from 'redis';
+import { User } from '../../interfaces/User';
+import { Issuer, TokenSet } from 'openid-client';
+import { auth } from 'express-openid-connect';
+import { Logger } from '../../interfaces/Logger';
 
 export class OidcMiddleware {
   private readonly clientId: string = config.get('services.idam.clientID');
@@ -9,21 +24,122 @@ export class OidcMiddleware {
   private readonly baseUrl: string = config.get('services.idam.url.dashboard');
   private readonly idamBaseUrl: string = config.get('services.idam.url.public');
   private readonly sessionSecret: string = config.get('session.secret');
+  private readonly accessRole: string = config.get('RBAC.access');
+  private readonly systemAccountUsername = config.get('services.idam.systemUser.username');
+  private readonly systemAccountPassword = config.get('services.idam.systemUser.password');
+
+  constructor(private readonly logger: Logger) {}
 
   public enableFor(app: Application): void {
-    app.use(
-      auth({
-        issuerBaseURL: this.idamBaseUrl + '/o',
-        baseURL: this.baseUrl,
-        clientID: this.clientId,
-        secret: this.sessionSecret,
-        clientSecret: this.clientSecret,
-        clientAuthMethod: 'client_secret_post',
-        authorizationParams: {
-          'response_type': 'code',
-          scope: this.clientScope
+    this.cacheSystemAccount(app);
+
+    app.use(auth({
+      issuerBaseURL: this.idamBaseUrl + '/o',
+      baseURL: this.baseUrl,
+      clientID: this.clientId,
+      secret: this.sessionSecret,
+      clientSecret: this.clientSecret,
+      clientAuthMethod: 'client_secret_post',
+      idpLogout: true,
+      authorizationParams: {
+        'response_type': 'code',
+        scope: this.clientScope
+      },
+      session: {
+        rollingDuration: 20 * (60 * 1000),
+        cookie: {
+          httpOnly: true,
+        },
+        rolling: true,
+        store: this.getSessionStore(app)
+      },
+      afterCallback: (req: Request, res: Response, session: Session) => {
+        const user = jwtDecode(session.id_token) as User;
+
+        if (!user.roles.includes(this.accessRole)) {
+          throw new HTTPError(http.HTTP_STATUS_FORBIDDEN);
         }
+
+        return { ...session, user };
+      }
+    }));
+
+    app.use((req: AuthedRequest, res: Response, next: NextFunction) => {
+      req.scope = req.app.locals.container.createScope().register({
+        userAxios: asValue(this.createAuthedAxiosInstance(req.appSession.access_token)),
+        api: asClass(IdamAPI)
+      });
+
+      if (!req.appSession.user.assignableRoles) {
+        return req.scope.cradle.api.getAssignableRoles(req.appSession.user.roles)
+          .then(assignableRoles => {
+            req.appSession.user.assignableRoles = assignableRoles;
+            next();
+          })
+          .catch(err => next(err));
+      }
+
+      return next();
+    });
+  }
+
+  private getSessionStore(app: Application): any {
+    const redisStore = ConnectRedis(session);
+    const fileStore = FileStoreFactory(session);
+
+    const redisHost: string = config.get('session.redis.host');
+    const redisPort: number = config.get('session.redis.port');
+    const redisPass: string = config.get('session.redis.key');
+
+    if (redisHost && redisPass) {
+      const client = createClient({
+        host: redisHost,
+        password: redisPass,
+        port: redisPort ?? 6380,
+        tls: true
+      });
+
+      app.locals.redisClient = client;
+      return new redisStore({ client });
+    }
+
+    return new fileStore({ path: '/tmp' });
+  }
+
+  private cacheSystemAccount = (app: Application): void => {
+    let delay = 10 * 60;
+
+    this.getSystemUserAccessToken()
+      .then(tokenSet => {
+        app.locals.container.register({
+          systemAxios: asValue(this.createAuthedAxiosInstance(tokenSet.access_token))
+        });
+
+        delay = tokenSet.expires_in/2;
+        this.logger.info('Refreshed system user token. Refreshing again in: ' + Math.floor(delay/60) + 'mins');
       })
-    );
+      .catch(() => this.logger.info('Failed to refresh system user token. Refreshing again in: ' + delay/60 + 'mins'))
+      .finally(() => setTimeout(this.cacheSystemAccount, delay * 1000, app));
+  }
+
+  private createAuthedAxiosInstance(accessToken: string): AxiosInstance {
+    return axios.create({
+      baseURL: config.get('services.idam.url.api'),
+      headers: {Authorization: 'Bearer ' + accessToken}
+    });
+  }
+
+  private async getSystemUserAccessToken(): Promise<TokenSet> {
+    return new (await Issuer.discover(this.idamBaseUrl + '/o'))
+      .Client({
+        'client_id': this.clientId,
+        'client_secret': this.clientSecret,
+        'token_endpoint_auth_method': 'client_secret_post'
+      }).grant({
+        'grant_type': 'password',
+        'username': this.systemAccountUsername,
+        'password': this.systemAccountPassword,
+        'scope': this.clientScope,
+      });
   }
 }
