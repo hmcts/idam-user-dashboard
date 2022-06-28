8b7b98591c402495b77699d409149303abf042d4
ashwini.venkatesha <ashwini.venkatesha@justice.gov.uk>
2022-06-28 14:31:38 +0100

    Changes added to reflect locked status in user details page

--- src/test/functional/lock-account-test.ts
+++ src/test/functional/lock-account-test.ts
@@ -4,16 +4,18 @@ import {createUserWithRoles} from './shared/testingSupportApi';
 
 Feature('Lock User Account');
 const DASHBOARD_USER_EMAIL = randomData.getRandomEmailAddress();
+const TEST_USER_EMAIL = randomData.getRandomEmailAddress();
 const AFTER_RESET_PASSWORD = testConfig.PASSWORD + randomData.getRandomString(2);
 
 BeforeSuite(async () => {
   await createUserWithRoles(DASHBOARD_USER_EMAIL, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, [testConfig.RBAC.access]);
+  await createUserWithRoles(TEST_USER_EMAIL, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, [testConfig.RBAC.access]);
 });
 
-Scenario('I as a user when try to sign in should get locked after 5 login attempts with wrong password and should be able to login once I reset my password', async ({I}) => {
+Scenario('User details page should reflect users locked status and active status when unlocked', async ({I}) => {
   I.amOnPage('/login');
   I.see('Sign in');
-  I.fillField('#username', DASHBOARD_USER_EMAIL);
+  I.fillField('#username', TEST_USER_EMAIL);
   I.fillField('#password', 'WrongPassword@5');
   I.click('Sign in');
   I.waitForText('Incorrect email or password');
@@ -31,13 +33,28 @@ Scenario('I as a user when try to sign in should get locked after 5 login attemp
   I.waitForText('There is a problem with your account login details');
   I.waitForText('Your account is locked due to too many unsuccessful attempts.');
   I.waitForText('You can reset your password to unlock your account.');
+
+  I.loginAs(DASHBOARD_USER_EMAIL, testConfig.PASSWORD);
+  I.waitForText('Manage an existing user');
+  I.click('Manage an existing user');
+  I.click('Continue');
+  I.waitForText('Please enter the email address, user ID or SSO ID of the user you wish to manage');
+  I.click('#search');
+  I.fillField('#search', TEST_USER_EMAIL);
+  I.click('Search');
+  I.waitForText('User Details');
+  I.see('This account has been temporarily locked due to multiple failed login attempts. The temporary lock will end in 60 minutes');
+  I.click('Sign out');
+
+  I.loginAs(TEST_USER_EMAIL, testConfig.PASSWORD);
+  I.waitForText('You can reset your password to unlock your account.');
   I.click('reset your password');
   I.waitForText('Reset your password');
-  I.fillField('#email', DASHBOARD_USER_EMAIL);
+  I.fillField('#email', TEST_USER_EMAIL);
   I.click('Submit');
   I.waitForText('Check your email');
 
-  const response = await I.extractUrlFromNotifyEmail(DASHBOARD_USER_EMAIL);
+  const response = await I.extractUrlFromNotifyEmail(TEST_USER_EMAIL);
   const resetLink = response.body.match(/https:\/\/.*/);
 
   I.amOnPage(resetLink.toString());
@@ -47,6 +64,16 @@ Scenario('I as a user when try to sign in should get locked after 5 login attemp
   I.fillField('#password2', AFTER_RESET_PASSWORD);
   I.click('Continue');
   I.waitForText('Your password has been changed');
-  I.loginAs(DASHBOARD_USER_EMAIL, AFTER_RESET_PASSWORD);
+
+  I.loginAs(DASHBOARD_USER_EMAIL, testConfig.PASSWORD);
   I.waitForText('Manage an existing user');
+  I.click('Manage an existing user');
+  I.click('Continue');
+  I.waitForText('Please enter the email address, user ID or SSO ID of the user you wish to manage');
+  I.click('#search');
+  I.fillField('#search', TEST_USER_EMAIL);
+  I.click('Search');
+  I.waitForText('User Details');
+  I.see('ACTIVE');
+  I.dontSee('This account has been temporarily locked due to multiple failed login attempts.');
 });
