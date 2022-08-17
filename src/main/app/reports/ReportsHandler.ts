61b905c905cfa623a59835e6a917b6dfc2c6c099
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-08-17 20:16:28 +0100

    SIDM-7839 - Updated report handler to add logging and CSV functionality

--- src/main/app/reports/ReportsHandler.ts
+++ src/main/app/reports/ReportsHandler.ts
@@ -4,7 +4,9 @@ import { createClient } from 'redis';
 import { User } from '../../interfaces/User';
 import { promisify } from 'util';
 import { v4 as uuidv4 } from 'uuid';
-import { Parser } from 'json2csv';
+import { parse } from 'json2csv';
+import { TelemetryClient } from 'applicationinsights';
+import { Logger } from '../../interfaces/Logger';
 
 type Store = {
   set: (reportUUID: string, users: User[]) => Promise<void>;
@@ -13,23 +15,36 @@ type Store = {
 
 export class ReportsHandler {
   private readonly store: Store;
-  private readonly reportTimeout = 30 * 60 * 1000;
+  private readonly reportTimeout = 30 * 60;
 
-  public constructor() {
+  public constructor(
+    private readonly logger: Logger,
+    private readonly telemetryClient: TelemetryClient
+  ) {
     this.store = this.getStore();
   }
 
-  public saveReport(users: User[]): Promise<string> {
+  public saveReport(users: User[]): Promise<string | void> {
     const uuid = uuidv4();
     return this.store
       .set(uuid, users)
-      .then(() => uuid);
+      .then(() => uuid)
+      .catch(e => {
+        this.logger.error(e);
+        this.telemetryClient.trackTrace(e);
+        throw new Error();
+      });
   }
 
   public getReport(reportUUID: string): Promise<string> {
     return this.store.get(reportUUID)
       .then((data: string) => JSON.parse(data))
-      .then((data: Record<string, string>) => new Parser().parse(data));
+      .then((data: User[]) => parse(data))
+      .catch(e => {
+        this.logger.error(e);
+        this.telemetryClient.trackTrace(e);
+        throw new Error();
+      });
   }
 
   private getStore(): Store {
@@ -37,34 +52,36 @@ export class ReportsHandler {
     const redisPort: number = config.get('session.redis.port');
     const redisPass: string = config.get('session.redis.key');
 
-    if (redisHost && redisPass) {
-      console.log('Using Redis Store');
+    if (redisHost) {
+      this.logger.info('Using Redis Store');
       return this.getRedisStore(redisHost, redisPort, redisPass);
     }
 
-    console.log('Using In Memory Store');
+    this.logger.info('Using In Memory Store');
     return this.getInMemoryStore();
   }
 
   private getRedisStore(redisHost: string, redisPort: number, redisPass: string): Store {
     const client = createClient({
       host: redisHost,
-      password: redisPass,
       port: redisPort ?? 6380,
-      tls: true
     });
 
     const setAsync = promisify(client.set).bind(client);
-    const expireAsync = promisify(client.expire).bind(client);
     const getAsync = promisify(client.get).bind(client);
 
     return {
       set: async (reportUUID: string, users: User[]): Promise<void> => {
-        await setAsync(reportUUID, JSON.stringify(users));
-        await expireAsync(reportUUID, this.reportTimeout);
+        return setAsync(reportUUID, JSON.stringify(users), 'EX', this.reportTimeout)
+          .catch((e: string) => {
+            throw new Error('Error saving report ' + reportUUID + ' from redis store - ' + e);
+          });
       },
       get: async (reportUUID: string): Promise<string> => {
-        return getAsync(reportUUID);
+        return getAsync(reportUUID)
+          .catch((e: string) => {
+            throw new Error('Error saving report ' + reportUUID + ' from redis store - ' + e);
+          });
       }
     };
   }
@@ -72,11 +89,17 @@ export class ReportsHandler {
   private getInMemoryStore(): Store {
     return {
       set: (reportUUID: string, users: User[]): Promise<void> => {
-        return fs.promises.writeFile('/' + reportUUID, JSON.stringify(users));
+        return fs.promises.writeFile('/' + reportUUID, JSON.stringify(users))
+          .catch(e => {
+            throw new Error('Error saving report ' + reportUUID + ' from in-memory store - ' + e);
+          });
       },
       get: (reportUUID: string): Promise<string> => {
         return fs.promises.readFile('/' + reportUUID)
-          .then(data => data.toString());
+          .then(data => data.toString())
+          .catch(e => {
+            throw new Error('Error getting report ' + reportUUID + ' from in-memory store - ' + e);
+          });
       }
     };
   }
