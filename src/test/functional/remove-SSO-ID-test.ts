b5a74b634560adcd9ec05e0c843e0c49a78f843d
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-10-24 12:29:41 +0100

    SIDM-8165 - Cleaned up remove-sso-id test

--- src/test/functional/remove-SSO-ID-test.ts
+++ src/test/functional/remove-SSO-ID-test.ts
@@ -6,45 +6,62 @@ import {
   createUserWithRoles,
   createUserWithSsoId
 } from './shared/testingSupportApi';
+import { User } from '../../main/interfaces/User';
 
 Feature('Remove SSO ID');
 
 const PARENT_ROLE = randomData.getRandomRole();
 const ASSIGNABLE_CHILD_ROLE = randomData.getRandomRole();
-const INDEPENDENT_CHILD_ROLE = randomData.getRandomRole();
-const PARENT_ROLE_EMAIL = randomData.getRandomEmailAddress();
-const REMOVABLE_SSO_USER_EMAIL = randomData.getRandomEmailAddress();
-const NON_REMOVABLE_SSO_USER_EMAIL = randomData.getRandomEmailAddress();
-let removableSsoUser;
-let nonRemovableSsoUser;
+let dashboardUser: User;
+let ssoUserA: User;
+let ssoUserB: User;
 
 BeforeSuite(async () => {
   await createAssignableRoles(PARENT_ROLE);
   await createAssignableRoles(ASSIGNABLE_CHILD_ROLE);
-  await createAssignableRoles(INDEPENDENT_CHILD_ROLE);
   // Assigning self role with the child role so the this user can also delete same level users
   await assignRolesToParentRole(PARENT_ROLE, [ASSIGNABLE_CHILD_ROLE, PARENT_ROLE]);
-  await createUserWithRoles(PARENT_ROLE_EMAIL, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, [testConfig.RBAC.access, PARENT_ROLE]);
-  removableSsoUser = await createUserWithSsoId(REMOVABLE_SSO_USER_EMAIL, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, [ASSIGNABLE_CHILD_ROLE], randomData.getRandomSSOId());
-  nonRemovableSsoUser = await createUserWithSsoId(NON_REMOVABLE_SSO_USER_EMAIL, testConfig.PASSWORD, testConfig.USER_FIRSTNAME, [INDEPENDENT_CHILD_ROLE], randomData.getRandomSSOId());
+
+  dashboardUser = await createUserWithRoles(
+    randomData.getRandomEmailAddress(),
+    testConfig.PASSWORD,
+    testConfig.USER_FIRSTNAME,
+    [testConfig.RBAC.access, PARENT_ROLE]
+  );
+
+  ssoUserA = await createUserWithSsoId(
+    randomData.getRandomEmailAddress(),
+    testConfig.PASSWORD,
+    testConfig.USER_FIRSTNAME,
+    [ASSIGNABLE_CHILD_ROLE],
+    randomData.getRandomSSOId()
+  );
+
+  ssoUserB = await createUserWithSsoId(
+    randomData.getRandomEmailAddress(),
+    testConfig.PASSWORD,
+    testConfig.USER_FIRSTNAME,
+    [ASSIGNABLE_CHILD_ROLE],
+    randomData.getRandomSSOId()
+  );
 });
 
 
 Scenario('I as a user should be able to remove sso from user account', async ({I}) => {
-  I.loginAs(PARENT_ROLE_EMAIL, testConfig.PASSWORD);
+  I.loginAs(dashboardUser.email, testConfig.PASSWORD);
   I.waitForText('Manage an existing user');
   I.click('Manage an existing user');
   I.click('Continue');
   I.waitForText('Please enter the email address, user ID or SSO ID of the user you wish to manage');
   I.click('#search');
-  I.fillField('#search', REMOVABLE_SSO_USER_EMAIL);
+  I.fillField('#search', ssoUserA.email);
   I.click('Search');
   I.waitForText('User Details');
-  I.see(REMOVABLE_SSO_USER_EMAIL);
+  I.see(ssoUserA.email);
   I.see('IdP User ID');
   I.see('Identity Provider');
-  I.see(removableSsoUser.ssoId);
-  I.see(removableSsoUser.ssoProvider);
+  I.see(ssoUserA.ssoId);
+  I.see(ssoUserA.ssoProvider);
   I.see('Remove SSO');
   I.click('Remove SSO');
   I.waitForText('Are you sure you want to remove single sign-on for this user? The user will need to reset their password.');
@@ -53,38 +70,38 @@ Scenario('I as a user should be able to remove sso from user account', async ({I
   I.waitForText('Single sign-on removed successfully');
   I.click('Return to user details');
   I.waitForText('User Details');
-  I.see(REMOVABLE_SSO_USER_EMAIL);
+  I.see(ssoUserA.email);
   I.dontSee('IdP User ID');
   I.dontSee('Identity Provider');
-  I.dontSee(removableSsoUser.ssoId);
-  I.dontSee(removableSsoUser.ssoProvider);
+  I.dontSee(ssoUserA.ssoId);
+  I.dontSee(ssoUserA.ssoProvider);
 }).tag('@CrossBrowser');
 
 Scenario('I as a user should be able to select no while removing sso', async ({I}) => {
-  I.loginAs(PARENT_ROLE_EMAIL, testConfig.PASSWORD);
+  I.loginAs(dashboardUser.email, testConfig.PASSWORD);
   I.waitForText('Manage an existing user');
   I.click('Manage an existing user');
   I.click('Continue');
   I.waitForText('Please enter the email address, user ID or SSO ID of the user you wish to manage');
   I.click('#search');
-  I.fillField('#search', REMOVABLE_SSO_USER_EMAIL);
+  I.fillField('#search', ssoUserB.email);
   I.click('Search');
   I.waitForText('User Details');
-  I.see(REMOVABLE_SSO_USER_EMAIL);
+  I.see(ssoUserB.email);
   I.see('IdP User ID');
   I.see('Identity Provider');
-  I.see(removableSsoUser.ssoId);
-  I.see(removableSsoUser.ssoProvider);
+  I.see(ssoUserB.ssoId);
+  I.see(ssoUserB.ssoProvider);
   I.see('Remove SSO');
   I.click('Remove SSO');
   I.waitForText('Are you sure you want to remove single sign-on for this user? The user will need to reset their password.');
   I.click('#confirmRadio-2');
   I.click('Continue');
   I.waitForText('User Details');
-  I.see(REMOVABLE_SSO_USER_EMAIL);
+  I.see(ssoUserB.email);
   I.see('IdP User ID');
   I.see('Identity Provider');
-  I.see(removableSsoUser.ssoId);
-  I.see(removableSsoUser.ssoProvider);
+  I.see(ssoUserB.ssoId);
+  I.see(ssoUserB.ssoProvider);
 }).tag('@CrossBrowser');
 
