e79ade1b0b6d58c5ea126aa7d1033fff513c436f
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-10-31 22:19:29 +0000

    SIDM-8165 - Added custom step to lock account

--- src/test/functional/custom-steps.ts
+++ src/test/functional/custom-steps.ts
@@ -14,6 +14,25 @@ export = function () {
       this.click('Sign out');
       this.see('Sign in');
     },
+    lockAccountOf: function (username) {
+      const LOGIN_ATTEMPT_LIMIT = 5;
+      this.amOnPage(LOGIN_URL);
+      this.see('Sign in');
+      this.fillField('#username', username);
+      this.fillField('#password', 'SOME_WRONG_PASSWORD');
+
+      for (let i = 0; i < LOGIN_ATTEMPT_LIMIT; i++) {
+        this.click('Sign in');
+
+        if (i === LOGIN_ATTEMPT_LIMIT - 1) {
+          this.see('There is a problem with your account login details');
+          this.see('Your account is locked due to too many unsuccessful attempts.');
+          this.see('You can reset your password to unlock your account.');
+        } else {
+          this.see('Incorrect email or password');
+        }
+      }
+    },
     gotoUserDetails: function (id: string) {
       this.amOnPage(MANAGER_USER_URL);
       this.fillField('#search', id);
