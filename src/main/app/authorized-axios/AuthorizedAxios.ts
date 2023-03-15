86496c9403d3f567651a8b82689bbc34f1afd550
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2023-03-15 13:09:26 +0000

    Re-added checks for if oauth.token object is undefined

--- src/main/app/authorized-axios/AuthorizedAxios.ts
+++ src/main/app/authorized-axios/AuthorizedAxios.ts
@@ -104,9 +104,9 @@ export class AuthorizedAxios extends Axios {
 
   private requestInterceptor = () => {
     this.interceptors.request.use(config => {
-      if (!config.url?.endsWith('/o/token')) {
+      if (this.oauth.token && !config.url?.endsWith('/o/token')) {
         config.headers = config.headers ?? {};
-        config.headers.Authorization = 'Bearer ' + this.oauth?.token?.raw;
+        config.headers.Authorization = 'Bearer ' + this.oauth.token.raw;
       }
 
       return config;
