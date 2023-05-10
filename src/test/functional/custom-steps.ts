816cfe75ccf5f36f4c6a65a262a98145080a8a23
Arun <arun.kumar@hmcts.net>
2023-05-10 07:20:07 +0100

    SIDM-8606 Added wait

--- src/test/functional/custom-steps.ts
+++ src/test/functional/custom-steps.ts
@@ -38,6 +38,7 @@ export = function () {
       this.amOnPage(MANAGER_USER_URL);
       this.fillField('#search', id);
       this.click('Search');
+      this.waitFor('User Details');
       this.see('User Details');
       this.see(id);
     }
