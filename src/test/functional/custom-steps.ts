9e93852433e5d19a06d36f322e8e6179f8a4fe9e
James Burke (IDAM) <james.burke@hmcts.net>
2023-07-24 12:27:15 +0100

    fragile

--- src/test/functional/custom-steps.ts
+++ src/test/functional/custom-steps.ts
@@ -9,6 +9,7 @@ export = function () {
       this.fillField('#username', username);
       this.fillField('#password', password);
       this.click('Sign in');
+      this.seeInCurrentUrl(testConfig.TEST_URL);
     },
     logout: function () {
       this.click('Sign out');
@@ -38,6 +39,7 @@ export = function () {
       this.amOnPage(MANAGER_USER_URL);
       this.fillField('#search', id);
       this.click('Search');
+      this.seeInCurrentUrl('/user/details');
       this.see('User Details');
       this.see(id);
     }
