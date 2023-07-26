12cd159cbb15658a2a432d0648663dda176aae6f
James Burke (IDAM) <james.burke@hmcts.net>
2023-07-26 11:14:42 +0100

    for pr

--- src/test/config.ts
+++ src/test/config.ts
@@ -16,7 +16,7 @@ export const config = {
     access: 'idam-user-dashboard--access'
   },
   TestHeadlessBrowser: process.env.TEST_HEADLESS ? process.env.TEST_HEADLESS === 'true' : true,
-  WaitForTimeout: 60009,
+  WaitForTimeout: 60008,
   helpers: {},
   plugins: {
     retryFailedStep: {
