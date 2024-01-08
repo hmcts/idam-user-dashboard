5ecb9f9dd3dc73031a32e278515284e9b5202298
James Burke (IDAM) <james.burke@hmcts.net>
2024-01-08 15:19:31 +0000

    report

--- src/test/best/codecept.conf.ts
+++ src/test/best/codecept.conf.ts
@@ -13,7 +13,7 @@ export const config: CodeceptJS.MainConfig = {
   },
   plugins: {
     allure: {
-      "enabled": true,
+      enabled: true,
       require: '@codeceptjs/allure-legacy'
     }
   },
