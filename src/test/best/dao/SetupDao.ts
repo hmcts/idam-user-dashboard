febf2d15de421db955ed81ef41bb251c89823483
James Burke (IDAM) <james.burke@hmcts.net>
2024-01-09 11:56:21 +0000

    auto login

--- src/test/best/dao/SetupDao.ts
+++ src/test/best/dao/SetupDao.ts
@@ -1,76 +1,74 @@
 const { I } = inject();
-const { faker } = require('@faker-js/faker');
 
 class SetupDAO {
 
-    testingToken : string;
+  testingToken : string;
 
-    setToken(tokenValue: string) {
-      this.testingToken = tokenValue;
-    }
+  setToken(tokenValue: string) {
+    this.testingToken = tokenValue;
+  }
   
-    async getToken() {
-      if (this.testingToken) {
-        console.log("testing token is set");
-        return this.testingToken;
-      }
-        
-      var tokenRsp = await I.sendPostRequest('https://idam-api.aat.platform.hmcts.net/o/token', { 
-        'grant_type':'client_credentials',
-        'client_id':'idam-functional-test-service',
-        'client_secret': process.env.FUNCTIONAL_TEST_SERVICE_CLIENT_SECRET,
-        'scope':'profile roles' ,
-      },
-      {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      });
-      I.seeResponseCodeIsSuccessful();
-      this.setToken(tokenRsp.data.access_token);
+  async getToken() {
+    if (this.testingToken) {
+      console.log('testing token is set');
       return this.testingToken;
+    }
+        
+    const tokenRsp = await I.sendPostRequest('https://idam-api.aat.platform.hmcts.net/o/token', { 
+      'grant_type':'client_credentials',
+      'client_id':'idam-functional-test-service',
+      'client_secret': process.env.FUNCTIONAL_TEST_SERVICE_CLIENT_SECRET,
+      'scope':'profile roles' ,
+    },
+    {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    });
+    I.seeResponseCodeIsSuccessful();
+    this.setToken(tokenRsp.data.access_token);
+    return this.testingToken;
   
+  }
+
+  async setupAdmin() {
+    if (codeceptjs.container.support('adminIdentity')) {
+      console.log('admin already setup');
+      return;
     }
 
-    async setupAdmin() {
+    console.log('setup admin');
 
-      if (codeceptjs.container.support('adminIdentity')) {
-        console.log('admin already setup');
-        return;
+    const testToken = await this.getToken();
+    I.amBearerAuthenticated(testToken);
+    const email = 'testadmin@admin.local';
+    const secret = process.env.SMOKE_TEST_USER_PASSWORD;
+    const adminRsp = await I.sendPostRequest('/test/idam/users', { 
+      'password': secret,
+      'user' : {
+        'email':email,
+        'forename':'admin',
+        'surname':'test',
+        'roleNames': [
+          'idam-user-dashboard--access'
+        ]
       }
-
-      console.log('setup admin');
-
-      var testToken = await this.getToken();
-      I.amBearerAuthenticated(testToken);
-      const email = 'testadmin@admin.local';
-      const secret = process.env.SMOKE_TEST_USER_PASSWORD;
-      var adminRsp = await I.sendPostRequest('/test/idam/users', { 
-        'password': secret,
-        'user' : {
-          'email':email,
-          'forename':'admin',
-          'surname':'test',
-          'roleNames': [
-            'idam-user-dashboard--access'
-          ]
-      }},
-      {
-        'Content-Type': 'application/json'
-      });
-      if (adminRsp.status == 409) {
-          I.seeResponseCodeIs(409);
-      } else {
-          I.seeResponseCodeIsSuccessful();
-      }
-      codeceptjs.container.append({
-        support: {
-          adminIdentity: {
-            email: email,
-            secret: secret
-          }
+    },
+    {
+      'Content-Type': 'application/json'
+    });
+    if (adminRsp.status == 409) {
+      I.seeResponseCodeIs(409);
+    } else {
+      I.seeResponseCodeIsSuccessful();
+    }
+    codeceptjs.container.append({
+      support: {
+        adminIdentity: {
+          email: email,
+          secret: secret
         }
-      });
-    }
-
+      }
+    });
+  }
 }
 
 export = new SetupDAO();
