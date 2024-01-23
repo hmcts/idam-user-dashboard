c8830e2673f0e4f9b2b03be263f6c2ca6b1ab452
James Burke <james.burke.cv@gmail.com>
2024-01-23 14:32:58 +0000

    Sidm 9038 v2 codecept (#800)

---
+++ src/test/v2/common/shared_config.ts
@@ -0,0 +1,78 @@
+const envConfig = require('config');
+
+export const shared_config = {
+  include: {},
+  helpers: {},
+  plugins: {},
+};
+
+shared_config.include = {
+  I: '../common/steps_file',
+  setupDAO: '../common/dao/SetupDao.ts'
+};
+
+shared_config.helpers = {
+  Playwright: {
+    browser: 'chromium',
+    url: process.env.TEST_URL || 'https://idam-user-dashboard.aat.platform.hmcts.net/',
+    show: false,
+    timeout: 20002,
+    bypassCSP: false,
+    capabilities: {}
+  },
+  REST: {
+    endpoint: envConfig.get('services.idam.url.testingSupportApi'),
+    timeout: 30000
+  },
+  JSONResponse: {},
+  ChaiWrapper: {
+    require: 'codeceptjs-chai'
+  },
+  ApiDataFactory: {
+    endpoint: envConfig.get('services.idam.url.testingSupportApi'),
+    cleanup: false,
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    onRequest: async (request) => {
+      const testToken = await codeceptjs.container.support('testingToken');
+      request.headers = { 'Authorization': 'bearer ' + testToken };
+    }, 
+    factories: {
+      user: {
+        factory: '../common/factories/users.ts',
+        create: (data) =>  ({ method: 'POST',  url: '/test/idam/users', data })
+      },
+    },
+    REST : {
+      timeout: 29999
+    }
+  },
+};
+
+shared_config.plugins = {
+  allure: {
+    enabled: true,
+    require: '@codeceptjs/allure-legacy'
+  },
+  autoLogin: {
+    enabled: true,
+    saveToFile: false,
+    inject: 'login',
+    users: {
+      admin: {
+        // loginAs function is defined in `steps_file.js`
+        login: (I) => {
+          const adminIdentity = codeceptjs.container.support('adminIdentity');
+          I.loginAs(adminIdentity.email, adminIdentity.secret);
+        },
+        // if we see manage users page,  we are logged in
+        check: (I) => {
+          I.amOnPage('/');
+          I.see('What do you want to do?');
+        }
+      }
+    }
+  }
+};
---
+++ src/test/v2/functional/steps.d.ts
@@ -0,0 +1,13 @@
+/// <reference types='codeceptjs' />
+type steps_file = typeof import('../common/steps_file');
+type setupDAO = typeof import('../common/dao/SetupDao');
+type ChaiWrapper = import('codeceptjs-chai');
+
+declare namespace CodeceptJS {
+  interface SupportObject { I: I, current: any, login: any, setupDAO: setupDAO }
+  interface Methods extends Playwright, REST, JSONResponse, ChaiWrapper, ApiDataFactory {}
+  interface I extends ReturnType<steps_file>, WithTranslation<Methods> {}
+  namespace Translation {
+    interface Actions {}
+  }
+}
