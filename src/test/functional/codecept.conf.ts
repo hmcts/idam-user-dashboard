17a32e65ae6d89a71410c7b35970750176bf2db7
James Burke (IDAM) <james.burke@hmcts.net>
2024-02-05 16:01:28 +0000

    clean up

--- src/test/functional/codecept.conf.ts
+++ src/test/functional/codecept.conf.ts
@@ -2,7 +2,7 @@ import {config as testConfig} from '../config';
 
 export const config: CodeceptJS.Config = {
   name: 'functional',
-  tests: './gen-test.ts',
+  tests: './*-test.ts',
   output: '../../../functional-output/functional/reports',
   helpers: testConfig.helpers,
   timeout: 59000,
