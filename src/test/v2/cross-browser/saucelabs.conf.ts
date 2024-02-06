da6b2c0ea22ba4077a669dbc1473de52ba3e3953
James Burke <james.burke.cv@gmail.com>
2024-02-06 10:09:30 +0000

    admins per worker (#828)

--- src/test/v2/cross-browser/saucelabs.conf.ts
+++ src/test/v2/cross-browser/saucelabs.conf.ts
@@ -4,10 +4,6 @@ import {event, container} from 'codeceptjs';
 const LATEST_MAC = 'macOS 10.15';
 const LATEST_WINDOWS = 'Windows 10';
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
 const setupConfig = {
   name: 'cross-browser',
   tests: '../functional/*_test.ts',
@@ -16,10 +12,10 @@ const setupConfig = {
   helpers: sharedConfig.helpers,
   plugins: sharedConfig.plugins,
   bootstrap() {
-    const sleepMs = getRandomInt(99) * 100;
-    setTimeout(function() {
-      console.log('Sleeping in bootstrap for ' + sleepMs + '(ms)');
-    }, sleepMs);
+    const {Playwright} = container.helpers();
+    const sauceName = Playwright.options.capabilities['sauce:options'].name;
+    console.log('bootstrapping for ' + sauceName);
+    process.env.TEST_ADMIN_EMAIL = 'test' + sauceName.replaceAll('_', '').toLowerCase() + '@admin.local';
   },
   multiple: {
     cross_browser: {
