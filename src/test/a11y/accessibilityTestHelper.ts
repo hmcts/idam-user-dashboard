f96b554207eb5866c55ded58a98b51bf039e36f8
chandkanuri <chand.kanuri@hmcts.net>
2024-02-11 00:13:13 +0000

    Amended tests

--- src/test/a11y/accessibilityTestHelper.ts
+++ src/test/a11y/accessibilityTestHelper.ts
@@ -1,15 +1,15 @@
-import { expect, Page } from "@playwright/test";
-import AxeBuilder from "@axe-core/playwright";
+import { expect, Page } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
 
 async function axeTest(page: Page): Promise<void> {
   const accessibilityScanResults = await new AxeBuilder({ page })
     .withTags([
-      "wcag2a",
-      "wcag2aa",
-      "wcag21a",
-      "wcag21aa",
-      "wcag22a",
-      "wcag22aa",
+      'wcag2a',
+      'wcag2aa',
+      'wcag21a',
+      'wcag21aa',
+      'wcag22a',
+      'wcag22aa',
     ])
     .analyze();
   expect(accessibilityScanResults.violations).toEqual([]);
