3029978f98f9b4dc3b88eb712962221cfe43e07d
chandkanuri <chand.kanuri@hmcts.net>
2024-02-12 16:14:38 +0000

    Amended tests

--- src/test/v2/functional/delete_user_test.ts
+++ src/test/v2/functional/delete_user_test.ts
@@ -12,11 +12,6 @@ Scenario('I as an admin can delete user successfully',  async ({ I }) => {
   I.navigateToManageUser(testUser.email);
   I.see(testUser.email, I.locateDataForTitle('Email'));
   I.seeElement(locate('button').withText('Delete user'));
-
-
-  I.runA11yCheck({ outputDir: 'a11y' });
-  I.checkA11y();
-
   I.click('Delete user');
   I.seeAfterClick('Are you sure you want to delete', 'h1');
   I.checkOption('#confirmRadio');
