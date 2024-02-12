70d695fb5abab3fc0fad4ccd81a4d562f971d012
chandkanuri <chand.kanuri@hmcts.net>
2024-02-12 11:24:44 +0000

    Amended tests

--- src/test/v2/common/shared_config.ts
+++ src/test/v2/common/shared_config.ts
@@ -22,14 +22,14 @@ shared_config.helpers = {
   },
   A11yHelper: {
     require: 'codeceptjs-a11y-helper',
-    axeOptions: {
-      runOnly: {
-        values: [
-          'wcag2a', 'wcag2aa', 'wcag2aaa', 'wcag21a', 'wcag21aa', 'wcag22aa',
-          'best-practice', 'wcag***', 'ACT', 'experimental', 'cat.*'
-        ],
-      },
-    },
+    // axeOptions: {
+    //   runOnly: {
+    //     values: [
+    //       'wcag2a', 'wcag2aa', 'wcag2aaa', 'wcag21a', 'wcag21aa', 'wcag22aa',
+    //       'best-practice', 'wcag***', 'ACT', 'experimental', 'cat.*'
+    //     ],
+    //   },
+    // },
     outputDir: 'hello',
     reportFileName: 'a11y-audit.html'
   },
