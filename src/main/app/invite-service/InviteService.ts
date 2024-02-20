871bb11a54e93696d3052e4e7d703e6096fb1210
Nikola Naydenov <nikolanaydenov@MacBook-Pro.local>
2024-02-20 14:10:37 +0000

    SIDM-9099 Correct Invitation Type

--- src/main/app/invite-service/InviteService.ts
+++ src/main/app/invite-service/InviteService.ts
@@ -7,6 +7,7 @@ import { Logger } from '../../interfaces/Logger';
 
 export class InviteService {
   private readonly INVITE_ENDPOINT: string = config.get('services.idam.endpoint.invite');
+  private readonly appointmentMapString: string = config.get('services.idam.appointmentMap');
   private readonly DEFAULT_ROLE = 'citizen';
 
   constructor(
@@ -40,6 +41,31 @@ export class InviteService {
   };
 
   public inviteUser = (invite: Invite, language = 'en') => {
-    return this.sendInvite(InvitationTypes.SELF_REGISTER, invite, language);
+    const APPOINTMENT_MAP = JSON.parse(this.appointmentMapString);
+    const matchedInvitationType: InvitationTypes =
+      this.getEmailAppointmentMapEntryAsInvitationType(invite.email, APPOINTMENT_MAP);
+    if (matchedInvitationType) {
+      return this.sendInvite(matchedInvitationType, invite, language)
+    }
+    return this.sendInvite(InvitationTypes.INVITE, invite, language);
   };
+
+  getEmailAppointmentMapEntryAsInvitationType(email: string, map: { [key: string]: string } ): InvitationTypes | undefined {
+    let matchedValue: string;
+    const emailDomainInMap = Object.keys(map).some(key => {
+      const isMatch = email.toLowerCase().includes(key.toLowerCase());
+      if (isMatch) {
+        matchedValue = map[key];
+      }
+      return isMatch;
+    });
+    if (!emailDomainInMap) {
+      return undefined;
+    } else {
+      const entries = Object.entries(InvitationTypes) as [keyof typeof InvitationTypes, string][];
+      const found = entries.find(([key, val]) => val === matchedValue);
+      return found ? InvitationTypes[found[0]] : undefined;
+    }
+  }
+
 }
