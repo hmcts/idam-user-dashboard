ce470cb3ac1fac83eb4183131f70e855ee8e932c
chandkanuri <chand.kanuri@hmcts.net>
2024-02-22 17:52:03 +0000

    SIDM-9020-Added Accessibility tests.

--- src/test/v2/accessibility/accessibility_test.ts
+++ src/test/v2/accessibility/accessibility_test.ts
@@ -10,7 +10,7 @@ Scenario('I as an admin can delete user successfully',  async ({ I }) => {
   const testUser = await I.haveUser();
   await I.navigateToManageUser(testUser.email);
 
-  I.runA11yCheck({ outputDir: 'functional-output/accessibility' });
+  I.runA11yCheck();
   I.checkA11y();
 });
 
@@ -18,6 +18,6 @@ Scenario('I as an admin should edit user details successfully',  async ({ I }) =
   const testUser = await I.haveUser();
   await I.navigateToEditUser(testUser.email);
 
-  I.runA11yCheck({ outputDir: 'functional-output/accessibility' });
+  I.runA11yCheck();
   I.checkA11y();
 });
