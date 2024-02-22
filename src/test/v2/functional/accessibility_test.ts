28169b0e07c699c5df39b32ba6edb4eb5dd049b9
chandkanuri <chand.kanuri@hmcts.net>
2024-02-22 16:20:20 +0000

    SIDM-9020-Added Accessibility tests.

--- src/test/v2/functional/accessibility_test.ts
+++ src/test/v2/functional/accessibility_test.ts
@@ -1,18 +1,25 @@
-import { faker } from '@faker-js/faker';
+// import { faker } from '@faker-js/faker';
 Feature('v2_accessibility_tests');
 
 Before(async ({ setupDAO, login }) => {
-
-    await setupDAO.setupAdmin();
-    login('admin');
+  await setupDAO.setupAdmin();
+  login('admin');
   
 });
 
 Scenario('I as an admin can delete user successfully',  async ({ I }) => {
-    const testUser = await I.haveUser();
-    await I.navigateToManageUser(testUser.email);
+  const testUser = await I.haveUser();
+  await I.navigateToManageUser(testUser.email);
 
-    I.runA11yCheck({ outputDir: 'functional-output/accessibility' });
-    I.checkA11y();
+  I.runA11yCheck({ outputDir: 'functional-output/accessibility' });
+  I.checkA11y();
+});
+
+Scenario('I as an admin should edit user details successfully',  async ({ I }) => {
+  const testUser = await I.haveUser();
+  await I.navigateToEditUser(testUser.email);
+
+  I.runA11yCheck({ outputDir: 'functional-output/accessibility' });
+  I.checkA11y();
 });
 
