9bed51b80eb29c50adda2bfcd27365b1e6aa523b
chandkanuri <chand.kanuri@hmcts.net>
2024-02-22 18:10:59 +0000

    SIDM-9020-Added Accessibility tests.

--- src/test/v2/accessibility/accessibility_test.ts
+++ src/test/v2/accessibility/accessibility_test.ts
@@ -3,10 +3,9 @@ Feature('v2_accessibility_tests');
 Before(async ({ setupDAO, login }) => {
   await setupDAO.setupAdmin();
   login('admin');
-  
 });
 
-Scenario('I as an admin can delete user successfully',  async ({ I }) => {
+Scenario('I am on manage user page',  async ({ I }) => {
   const testUser = await I.haveUser();
   await I.navigateToManageUser(testUser.email);
 
@@ -14,9 +13,30 @@ Scenario('I as an admin can delete user successfully',  async ({ I }) => {
   I.checkA11y();
 });
 
-Scenario('I as an admin should edit user details successfully',  async ({ I }) => {
+Scenario('I am on search user page',  async ({ I }) => {
+  await I.navigateToSearchUser();
+
+  I.runA11yCheck();
+  I.checkA11y();
+});
+
+Scenario('I am on edit user page',  async ({ I }) => {
   const testUser = await I.haveUser();
   await I.navigateToEditUser(testUser.email);
+
+  I.runA11yCheck();
+  I.checkA11y();
+});
+
+Scenario('I am on generate user report page',  async ({ I }) => {
+  await I.navigateToGenerateReport();
+
+  I.runA11yCheck();
+  I.checkA11y();
+});
+
+Scenario('I am on add a new user page',  async ({ I }) => {
+  await I.navigateToRegisterUser();
 
   I.runA11yCheck();
   I.checkA11y();
