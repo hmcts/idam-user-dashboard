0b3cc7da943f448b9a7ac63347ba8a23f722bd71
chandkanuri <chand.kanuri@hmcts.net>
2024-02-26 15:11:45 +0000

    SIDM-9020-Added Accessibility tests.

--- src/test/v2/accessibility/accessibility_test.ts
+++ src/test/v2/accessibility/accessibility_test.ts
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 Feature('v2_accessibility_tests');
 
 Before(async ({ setupDAO, login }) => {
@@ -11,10 +13,15 @@ Scenario('I am on manage user page',  async ({ I }) => {
 
 
   const title = await I.grabTitle();
-
   console.log('Title is.....999999....'+title);
 
-  I.runA11yCheck();
+  const accessibilityReport = await I.runA11yCheck('html');
+  const customReportName = 'custom-a11y-report11111.json';
+
+  fs.writeFileSync(customReportName, JSON.stringify(accessibilityReport, null, 2));
+
+
+  //I.runA11yCheck();
   I.checkA11y();
 });
 
