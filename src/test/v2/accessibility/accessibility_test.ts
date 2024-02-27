9477fa0e611e5a3cf6aab88af9c3c2f54fa8b81c
chandkanuri <chand.kanuri@hmcts.net>
2024-02-27 16:23:36 +0000

    SIDM-9020-Added Accessibility tests.

--- src/test/v2/accessibility/accessibility_test.ts
+++ src/test/v2/accessibility/accessibility_test.ts
@@ -16,7 +16,7 @@ Scenario('I am on manage user page',  async ({ I }) => {
   // I.runA11yCheck({ reportFileName: 'chand123.html' });
 
 
-  I.runA11yCheck();
+  I.runA11yCheck({ reportFileName: 'manage-user.html' });
   I.checkA11y();
 
 
