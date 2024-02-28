cbb671b689f9ba866c8240fcc644c78e29abfcf1
chandkanuri <chand.kanuri@hmcts.net>
2024-02-28 07:31:33 +0000

    SIDM-9020-Added Accessibility tests.

--- src/test/a11y/aggregateAccessibilityResults.js
+++ src/test/a11y/aggregateAccessibilityResults.js
@@ -8,96 +8,36 @@ function aggregateAccessibilityResults() {
 
 
 
-    const regexPattern = /manage-user\.html$/;
-
-
-    findAndModifyHTML(regexPattern, 'New Heading Test');
 
 
 
 
+    fs.readdir(htmlFilesDir, (err, files) => {
+        if (err) {
+            console.error('Error reading from accessibility results directory - ', err);
+            return;
+        }
 
+        const filteredFiles = files.filter(file => file.endsWith('_a11y-audit.html'));
 
+        let htmlContent = '';
+        filteredFiles.forEach(file => {
+            const filePath = path.join(htmlFilesDir, file);
+            const content = fs.readFileSync(filePath, 'utf8');
+            htmlContent += content;
+        });
 
-
-
-
-
-
-
-
-
-
-    // fs.readdir(htmlFilesDir, (err, files) => {
-    //     if (err) {
-    //         console.error('Error reading from accessibility results directory - ', err);
-    //         return;
-    //     }
-
-    //     const filteredFiles = files.filter(file => file.endsWith('_a11y-audit.html'));
-
-    //     let htmlContent = '';
-    //     filteredFiles.forEach(file => {
-    //         const filePath = path.join(htmlFilesDir, file);
-    //         const content = fs.readFileSync(filePath, 'utf8');
-    //         htmlContent += content;
-    //     });
-
-    //     fs.writeFile(outputFile, htmlContent, (err) => {
-    //         if (err) {
-    //             console.error('Error writing output file - ', err);
-    //             return;
-    //         }
-    //         console.log('Accessibility results aggregated successfully');
-    //     });
-    // });
+        fs.writeFile(outputFile, htmlContent, (err) => {
+            if (err) {
+                console.error('Error writing output file - ', err);
+                return;
+            }
+            console.log('Accessibility results aggregated successfully');
+        });
+    });
 }
 
 
-function findAndModifyHTML(regexPattern, newHeading) {
-    const directory = 'functional-output/accessibility';
-  
-    console.log('I am here 000000000.........');
-  
-    fs.readdir(directory, (err, files) => {
-      if (err) {
-        console.error('Error reading directory amend heading:', err);
-        return;
-      }
-  
-      const filenamePattern = new RegExp(regexPattern);
-  
-      files.forEach(filename => {
-        console.log('I am here XXXXXX.........'+filename);
-        if (filenamePattern.test(filename)) {
-  
-          console.log('I am here 1111111.........');
-          const filePath = path.join(directory, filename);
-          fs.readFile(filePath, 'utf8', (err, data) => {
-            if (err) {
-              console.error(`Error reading file change heading ${filename}:`, err);
-              return;
-            }
-  
-            console.log('I am here 222222222.........');
-  
-            const modifiedContent = data.replace(/<h3>.*?<\/h3>/s, `<h3>${newHeading}</h3>`);
-  
-            console.log('I am here 3333333333.........');
-  
-            fs.writeFile(filePath, modifiedContent, 'utf8', err => {
-  
-              console.log('I am here 444444444.........');
-              if (err) {
-                console.error(`Error writing to file ${filename}:`, err);
-                return;
-              }
-              console.log(`Modified heading in ${filename}`);
-            });
-          });
-        }
-      });
-    });
-  }
+
 
 aggregateAccessibilityResults();
---
+++ src/test/a11y/amendAccessibilityReport.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const path = require('path');
+const htmlFilesDir = 'functional-output/accessibility';
+
+
+function amendAccessibilityReport() {
+
+
+    findAndModifyHTML(/manage-user-a11y-audit\.html$/, 'AXE Accessibility Results - Manage User');
+    findAndModifyHTML(/search-user-a11y-audit\.html$/, 'AXE Accessibility Results - Search User');
+    findAndModifyHTML(/edit-user-a11y-audit\.html$/, 'AXE Accessibility Results - Edit User');
+    findAndModifyHTML(/generate-user-report-a11y-audit\.html$/, 'AXE Accessibility Results - Generate User Report');
+    findAndModifyHTML(/add-new-user-a11y-audit\.html$/, 'AXE Accessibility Results - Add New User');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+function findAndModifyHTML(regexPattern, newHeading) {
+  
+    console.log('I am here 000000000.........');
+  
+    fs.readdir(htmlFilesDir, (err, files) => {
+      if (err) {
+        console.error('Error reading directory amend heading:', err);
+        return;
+      }
+  
+      const filenamePattern = new RegExp(regexPattern);
+  
+      files.forEach(filename => {
+        console.log('I am here XXXXXX.........'+filename);
+        if (filenamePattern.test(filename)) {
+  
+          console.log('I am here 1111111.........');
+          const filePath = path.join(htmlFilesDir, filename);
+          fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+              console.error(`Error reading file change heading ${filename}:`, err);
+              return;
+            }
+  
+            console.log('I am here 222222222.........');
+  
+            const modifiedContent = data.replace(/<h3>.*?<\/h3>/s, `<h3>${newHeading}</h3>`);
+  
+            console.log('I am here 3333333333.........');
+  
+            fs.writeFile(filePath, modifiedContent, 'utf8', err => {
+  
+              console.log('I am here 444444444.........');
+              if (err) {
+                console.error(`Error writing to file ${filename}:`, err);
+                return;
+              }
+              console.log(`Modified heading in ${filename}`);
+            });
+          });
+        }
+      });
+    });
+  }
+
+  amendAccessibilityReport();
