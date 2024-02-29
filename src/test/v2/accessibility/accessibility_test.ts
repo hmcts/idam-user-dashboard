e6b8c7a61e32d8a9cb48ca9c7dd3a2b2a22add6a
chandkanuri <chand.kanuri@hmcts.net>
2024-02-29 12:53:45 +0000

    SIDM-9020-Added Accessibility tests.

--- src/test/v2/accessibility/accessibility_test.ts
+++ src/test/v2/accessibility/accessibility_test.ts
@@ -10,14 +10,12 @@ Scenario('I am on manage user page',  async ({ I }) => {
   await I.navigateToManageUser(testUser.email);
 
   I.runA11yCheck({ reportFileName: 'manage-user-a11y-audit.html' });
-  I.checkA11y();
 });
 
 Scenario('I am on search user page',  async ({ I }) => {
   await I.navigateToSearchUser();
 
   I.runA11yCheck({ reportFileName: 'search-user-a11y-audit.html' });
-  I.checkA11y();
 });
 
 Scenario('I am on edit user page',  async ({ I }) => {
@@ -25,19 +23,20 @@ Scenario('I am on edit user page',  async ({ I }) => {
   await I.navigateToEditUser(testUser.email);
 
   I.runA11yCheck({ reportFileName: 'edit-user-a11y-audit.html' });
-  I.checkA11y();
 });
 
 Scenario('I am on generate user report page',  async ({ I }) => {
   await I.navigateToGenerateReport();
 
   I.runA11yCheck({ reportFileName: 'generate-user-report-a11y-audit.html' });
-  I.checkA11y();
 });
 
 Scenario('I am on add a new user page',  async ({ I }) => {
   await I.navigateToRegisterUser();
 
   I.runA11yCheck({ reportFileName: 'add-new-user-a11y-audit.html' });
+});
+
+After(async ({ I }) => {
   I.checkA11y();
 });
