7854f034f4d6c9c48177d721332409a902e06cc3
chandkanuri <chand.kanuri@hmcts.net>
2024-03-01 17:36:39 +0000

    SIDM-9020-Added Accessibility tests.

--- src/test/a11y/amendAccessibilityReport.js
+++ src/test/a11y/amendAccessibilityReport.js
@@ -1,62 +1,44 @@
 const fs = require('fs');
 const path = require('path');
+const directory = 'functional-output/accessibility';
+const regexPattern = /.*-a11y-audit(?=\.)/;
 
 
 function amendAccessibilityReport() {
-    // amendHeading(/.*manage-user-a11y-audit(?=\.)/, 'Manage User Page - Accessibility Results');
-    // amendHeading(/.*search-user-a11y-audit(?=\.)/, 'Search User Page - Accessibility Results');
-    // amendHeading(/.*edit-user-a11y-audit(?=\.)/, 'Edit User Page - Accessibility Results');
-    // amendHeading(/.*generate-user-report-a11y-audit(?=\.)/, 'Generate User Report Page - Accessibility Results');
-    // amendHeading(/.*add-new-user-a11y-audit(?=\.)/, 'Add New User Page - Accessibility Results');
 
-    // amendHeading(/.*manage-user-a11y-audit(?=\.)/);
-    // amendHeading(/.*search-user-a11y-audit(?=\.)/);
-    // amendHeading(/.*edit-user-a11y-audit(?=\.)/);
-    // amendHeading(/.*generate-user-report-a11y-audit(?=\.)/);
-    // amendHeading(/.*add-new-user-a11y-audit(?=\.)/);
+  fs.readdir(directory, (err, files) => {
+    if (err) {
+      console.error('Error reading accessibility results directory - ', err);
+      return;
+    }
 
-    //amendHeading(/.*-a11y-audit(?=\.)/);
+    const filenamePattern = new RegExp(regexPattern);
 
-    amendHeading(/^.*?_(.*?)-a11y-audit.html$/);
-}
+    files.forEach(filename => {
+      if (filenamePattern.test(filename)) {
+        const filePath = path.join(directory, filename);
+        fs.readFile(filePath, 'utf8', (err, data) => {
+          if (err) {
+            console.error(`Error reading accessibility results file ${filename} - `, err);
+            return;
+          }
 
+          const filenameRegex = /^.*?_(.*?)-a11y-audit.html$/;
+          const match = filenameRegex.exec(filename);
+          const extractedString = match[1];
+          const modifiedContent = data.replace(/<h3>.*?<\/h3>/s, `<h3>${extractedString} Page - Accessibility Results</h3>`);
 
-function amendHeading(regexPattern) {
-    const directory = 'functional-output/accessibility';
-  
-    fs.readdir(directory, (err, files) => {
-      if (err) {
-        console.error('Error reading accessibility results directory - ', err);
-        return;
-      }
-  
-      const filenamePattern = new RegExp(regexPattern);
-  
-      files.forEach(filename => {
-        if (filenamePattern.test(filename)) {
-          const filePath = path.join(directory, filename);
-          fs.readFile(filePath, 'utf8', (err, data) => {
+          fs.writeFile(filePath, modifiedContent, 'utf8', err => {
             if (err) {
-              console.error(`Error reading accessibility results file ${filename} - `, err);
+              console.error(`Error writing to file ${filename}:`, err);
               return;
             }
-
-            //const regex = /^.*?_(.*?)-a11y-audit.html$/;
-            const match = regexPattern.exec(filename);
-            const extractedString = match[1];
-            const modifiedContent = data.replace(/<h3>.*?<\/h3>/s, `<h3>${extractedString} Page - Accessibility Results</h3>`);
-  
-            fs.writeFile(filePath, modifiedContent, 'utf8', err => {
-              if (err) {
-                console.error(`Error writing to file ${filename}:`, err);
-                return;
-              }
-              console.log(`Modified heading in ${filename}`);
-            });
+            console.log(`Modified heading in ${filename}`);
           });
-        }
-      });
+        });
+      }
     });
-  }
+  });
+}
 
-  amendAccessibilityReport();
+amendAccessibilityReport();
