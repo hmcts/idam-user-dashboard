1464401225245de619d35479141b22286970d2a2
Nikola Naydenov <nikolanaydenov@MacBook-Pro.local>
2024-05-15 10:42:28 +0100

    That callback does not work

--- src/main/modules/health/index.ts
+++ src/main/modules/health/index.ts
@@ -18,12 +18,6 @@ export class HealthCheck {
     }));
 
     const healthOptions = {
-      callback: (err: Error, res: Request): Promise<void> => {
-        if (err) {
-          console.log('Health check failed!');
-        }
-        return res.body.status == 'good' ? healthcheck.up() : healthcheck.down();
-      },
       timeout: config.get('health.timeout'),
       deadline: config.get('health.deadline'),
     };
