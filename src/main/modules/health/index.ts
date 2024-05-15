0443b436532e4aaf0bc51ec5415ccedf499520b7
nikola-naydenov-hmcts <nikola.naydenov@hmcts.net>
2024-05-15 11:32:33 +0100

    Fix healthcheck options setup (#885)
    * Fix healthcheck options setup
    ---------
    
    Co-authored-by: Nikola Naydenov <nikolanaydenov@MacBook-Pro.local>

--- src/main/modules/health/index.ts
+++ src/main/modules/health/index.ts
@@ -1,7 +1,6 @@
 import { Application } from 'express';
 import os from 'os';
 import { infoRequestHandler } from '@hmcts/info-provider';
-import { Request } from 'express';
 import config from 'config';
 const healthcheck = require('@hmcts/nodejs-healthcheck');
 
@@ -17,17 +16,9 @@ export class HealthCheck {
       info: {},
     }));
 
-    const healthOptions = () => {
-      return {
-        callback: (err: Error, res: Request): Promise<void> => {
-          if (err) {
-            console.log('Health check failed!');
-          }
-          return res.body.status == 'good' ? healthcheck.up() : healthcheck.down();
-        },
-        timeout: config.get('health.timeout'),
-        deadline: config.get('health.deadline'),
-      };
+    const healthOptions = {
+      timeout: config.get('health.timeout'),
+      deadline: config.get('health.deadline'),
     };
 
     const healthCheckConfig = {
