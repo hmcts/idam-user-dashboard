e319eb278946c2ade84922ca6d5105c786644611
James Burke (IDAM) <james.burke@hmcts.net>
2024-06-14 15:21:59 +0100

    trace

--- src/main/controllers/UserEditController.ts
+++ src/main/controllers/UserEditController.ts
@@ -38,12 +38,8 @@ export class UserEditController extends RootController {
 
   @asyncError
   public post(req: AuthedRequest, res: Response) {
-    if (appInsights) {
-      const client = appInsights.defaultClient;
-      if (client) {
-        client.trackEvent({name: 'EditUser', properties: {editUserId: '' + req.body._userId}});
-      }
-    }
+    const client = appInsights.defaultClient;
+    client.trackEvent({name: 'EditUser', properties: {editUserId: '' + req.body._userId}});
     return req.scope.cradle.api.getUserById(req.body._userId)
       .then(user => {
         const roleAssignments = constructUserRoleAssignments(req.idam_user_dashboard_session.user.assignableRoles, user.roles);
