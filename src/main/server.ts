4602f0b6a34b986f199f497c890efe213d18435a
James Burke (IDAM) <james.burke@hmcts.net>
2024-07-09 17:18:53 +0100

    update

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,7 +3,7 @@ import { Logger } from '@hmcts/nodejs-logging';
 import { app } from './app';
 
 const logger = Logger.getLogger('server');
-console.log('server logger is ' + logger);
+console.log('server logger level is ' + logger.level);
 logger.warn('server.ts: log level is' + logger.level);
 
 const port: number = parseInt(process.env.PORT, 10) || 3100;
