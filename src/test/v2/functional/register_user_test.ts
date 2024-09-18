6c3c51b0eeb2609db02d50e3bb5f5be75c5555c1
James Burke <james.burke.cv@gmail.com>
2024-09-18 09:21:18 +0100

    SIDM-9380 wait for visible (#960)

--- src/test/v2/functional/register_user_test.ts
+++ src/test/v2/functional/register_user_test.ts
@@ -132,9 +132,11 @@ Scenario('I as an admin can search for roles to add', async ({ I, setupDAO }) =>
   I.uncheckOption('#hide-disabled');
 
   I.fillField('#roles__search-box', adminRole.name);
+  await tryTo(() => I.waitForVisible(I.locateRoleContainer(adminRole.name), 3));
   await I.retry({ retries: 9, minTimeout: 250 }).seeIsNotHidden(I.locateRoleContainer(adminRole.name));
   
   I.fillField('#roles__search-box', 'iud-filter-role-');
+  await tryTo(() => I.waitForInvisible(I.locateRoleContainer(adminRole.name), 3));
   await I.retry({ retries: 9, minTimeout: 250 }).seeIsHidden(I.locateRoleContainer(adminRole.name));
 
   const roleCheckboxes = await I.grabValueFromAll(locate('//div[@class=\'govuk-checkboxes__item\' and not(@hidden)]/input[@name=\'roles\']'));
