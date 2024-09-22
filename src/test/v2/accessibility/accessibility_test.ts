95a413e663c4e727abebaf30124e537339798056
chandkanuri <chand.kanuri@hmcts.net>
2024-09-22 23:00:50 +0100

    Test accessibility report.

--- src/test/v2/accessibility/accessibility_test.ts
+++ src/test/v2/accessibility/accessibility_test.ts
@@ -18,21 +18,21 @@ Scenario('I am on search user page',  async ({ I }) => {
   I.checkA11y('search-user-a11y-audit.html');
 });
 
-// Scenario('I am on edit user page',  async ({ I }) => {
-//   const testUser = await I.haveUser();
-//   await I.navigateToEditUser(testUser.email);
+Scenario('I am on edit user page',  async ({ I }) => {
+  const testUser = await I.haveUser();
+  await I.navigateToEditUser(testUser.email);
 
-//   I.checkA11y('edit-user-a11y-audit.html');
-// });
+  I.checkA11y('edit-user-a11y-audit.html');
+});
 
-// Scenario('I am on generate user report page',  async ({ I }) => {
-//   await I.navigateToGenerateReport();
+Scenario('I am on generate user report page',  async ({ I }) => {
+  await I.navigateToGenerateReport();
 
-//   I.checkA11y('generate-user-report-a11y-audit.html' );
-// });
+  I.checkA11y('generate-user-report-a11y-audit.html' );
+});
 
-// Scenario('I am on add a new user page',  async ({ I }) => {
-//   await I.navigateToRegisterUser();
+Scenario('I am on add a new user page',  async ({ I }) => {
+  await I.navigateToRegisterUser();
 
-//   I.checkA11y('add-new-user-a11y-audit.html');
-// });
+  I.checkA11y('add-new-user-a11y-audit.html');
+});
