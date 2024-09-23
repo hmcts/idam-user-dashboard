65f2aba79ec356eea8c6e645baa03896e40bf2f5
chandkanuri <chand.kanuri@hmcts.net>
2024-09-23 23:05:54 +0100

    Test accessibility report.

--- src/test/a11y/amendAccessibilityReport.js
+++ src/test/a11y/amendAccessibilityReport.js
@@ -13,16 +13,18 @@ function amendAccessibilityReport() {
     const filenamePattern = new RegExp(regexPattern);
     let auditFileFound = false;
 
-    const customHeading = 'Custom Heading Value';
-
     files.forEach(filename => {
       if (filenamePattern.test(filename)) {
         auditFileFound = true;
+        let pageTitle = convertFileNameToTitle(filename);
+
+        console.log('pageTitle....11111.....'+pageTitle);
+
         // const filePath = path.join(directory, filename);
 
         // const fileContent = fs.readFile(filePath, 'utf-8');
 
-        // const updatedContent = fileContent.replace(/<h3>(.*?)<\/h3>/g, `<h3>${customHeading}</h3>`);
+        // const updatedContent = fileContent.replace(/<h3>(.*?)<\/h3>/g, `<h3>${pageTitle}</h3>`);
 
         // fs.writeFile(filePath, updatedContent, 'utf-8');
 
@@ -36,6 +38,14 @@ function amendAccessibilityReport() {
       console.log('No accessibility audit files found.');
     }
   });
+}
+
+function convertFileNameToTitle(fileName) {
+  const cleanFileName = fileName.replace(/^\d+_|\_a11y-audit|\.html$/g, '');
+  const spacedFileName = cleanFileName.replace(/-/g, ' ');
+  let title = spacedFileName.replace(/\b\w/g, (char) => char.toUpperCase());
+  title = title.replace(/A11y Audit/i, 'Page');
+  return title;
 }
 
 amendAccessibilityReport();
