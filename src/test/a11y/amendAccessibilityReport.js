d7bd87341885e3f5c304a409601f12cbb86362cb
chandkanuri <chand.kanuri@hmcts.net>
2024-09-23 23:15:00 +0100

    Test accessibility report.

--- src/test/a11y/amendAccessibilityReport.js
+++ src/test/a11y/amendAccessibilityReport.js
@@ -12,21 +12,34 @@ function amendAccessibilityReport() {
 
     const filenamePattern = new RegExp(regexPattern);
     let auditFileFound = false;
+    const oldHeading = 'AXE Accessibility Results';
 
     files.forEach(filename => {
       if (filenamePattern.test(filename)) {
         auditFileFound = true;
         let pageTitle = convertFileNameToTitle(filename);
+        const filePath = path.join(directory, filename);
+
+        console.log('filename....000000.....'+filename);
 
         console.log('pageTitle....11111.....'+pageTitle);
 
-        // const filePath = path.join(directory, filename);
+        console.log('filePath....222222.....'+filePath);
 
-        // const fileContent = fs.readFile(filePath, 'utf-8');
+        const fileContent = fs.readFile(filePath, 'utf-8');
 
-        // const updatedContent = fileContent.replace(/<h3>(.*?)<\/h3>/g, `<h3>${pageTitle}</h3>`);
+        const updatedContent = fileContent.replace(
+          new RegExp(`<h3>\\s*${oldHeading}\\s*</h3>`, 'g'),
+          `<h3>${pageTitle}</h3>`
+      );
 
-        // fs.writeFile(filePath, updatedContent, 'utf-8');
+      fs.writeFile(filePath, updatedContent, 'utf-8');
+      console.log(`Successfully updated <h3> heading in file: ${filePath}`);
+
+
+
+
+
 
       }
     });
