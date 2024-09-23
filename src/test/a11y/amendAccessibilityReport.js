2299590691cbb26724abb30f88cb2a64da34e2dd
chandkanuri <chand.kanuri@hmcts.net>
2024-09-23 23:40:22 +0100

    Fixed accessibility report.

--- src/test/a11y/amendAccessibilityReport.js
+++ src/test/a11y/amendAccessibilityReport.js
@@ -12,28 +12,38 @@ function amendAccessibilityReport() {
 
     const filenamePattern = new RegExp(regexPattern);
     let auditFileFound = false;
-    const oldHeading = 'AXE Accessibility Results';
 
     files.forEach(filename => {
       if (filenamePattern.test(filename)) {
         auditFileFound = true;
-        let pageTitle = convertFileNameToTitle(filename);
+
         const filePath = path.join(directory, filename);
+        fs.readFile(filePath, 'utf8', (err, data) => {
+          if (err) {
+            console.error(`Error reading accessibility results file ${filename} - `, err);
+            process.exit(1);
+          }
 
-        console.log('filename....000000.....'+filename);
+          const filenameRegex = /^.*?_(.*?)-a11y-audit.html$/;
+          const match = filenameRegex.exec(filename);
+          if (match) {
+            const extractedString = match[1];
+            const convertedString = extractedString.replace(/-/g, ' ').replace(/\b\w/g, c => c.toUpperCase());
 
-        console.log('pageTitle....11111.....'+pageTitle);
+            const modifiedContent = data.replace(/<h3>.*?<\/h3>/s, `<h3>${convertedString} Page - Accessibility Results</h3>`);
 
-        console.log('filePath....222222.....'+filePath);
-
-        const fileContent = fs.readFile(filePath, 'utf-8');
-
-        const updatedContent = fileContent.replace(new RegExp(`<h3>\\s*${oldHeading}\\s*</h3>`, 'g'), `<h3>${pageTitle}</h3>`);
-
-        fs.writeFile(filePath, updatedContent, 'utf-8');
-        console.log(`Successfully updated <h3> heading in file: ${filePath}`);
+            fs.writeFile(filePath, modifiedContent, 'utf8', err => {
+              if (err) {
+                console.error(`Error writing to file ${filename}:`, err);
+                process.exit(1);
+              }
+              console.log(`Modified heading in ${filename}`);
+            });
+          }
+        });
       }
     });
+
 
     if (auditFileFound) {
       console.error('Accessibility audit file(s) found, breaking the pipeline.');
@@ -42,14 +52,6 @@ function amendAccessibilityReport() {
       console.log('No accessibility audit files found.');
     }
   });
-}
-
-function convertFileNameToTitle(fileName) {
-  const cleanFileName = fileName.replace(/^\d+_|\_a11y-audit|\.html$/g, '');
-  const spacedFileName = cleanFileName.replace(/-/g, ' ');
-  let title = spacedFileName.replace(/\b\w/g, (char) => char.toUpperCase());
-  title = title.replace(/A11y Audit/i, 'Page');
-  return title;
 }
 
 amendAccessibilityReport();
