0bff20faf4005882baa41f4738850b62208a42d3
Nikola Naydenov <nikolanaydenov@MacBook-Pro.local>
2024-10-02 12:04:26 +0100

    Fix linting

--- src/main/modules/helmet/index.ts
+++ src/main/modules/helmet/index.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
 import helmet from 'helmet';
 
-type ReferrerPolicyToken = "no-referrer" | "no-referrer-when-downgrade" | "same-origin" | "origin" | "strict-origin" | "origin-when-cross-origin" | "strict-origin-when-cross-origin" | "unsafe-url" | ""
+type ReferrerPolicyToken = 'no-referrer' | 'no-referrer-when-downgrade' | 'same-origin' | 'origin' | 'strict-origin' | 'origin-when-cross-origin' | 'strict-origin-when-cross-origin' | 'unsafe-url' | ''
 
 export interface HelmetConfig {
   referrerPolicy: ReferrerPolicyToken;
