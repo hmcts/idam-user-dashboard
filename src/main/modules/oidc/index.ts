5456ed73177164fbdc7a59e7ffb98fec599d04ab
Nikola Naydenov <nikolanaydenov@MacBook-Pro.local>
2024-10-02 10:30:16 +0100

    Fix linting

--- src/main/modules/oidc/index.ts
+++ src/main/modules/oidc/index.ts
@@ -1,10 +1,7 @@
 import { Application, NextFunction, Request, Response } from 'express';
 import config from 'config';
 import { AuthedRequest } from '../../interfaces/AuthedRequest';
-import { asClass, asValue } from 'awilix';
-import { IdamAPI } from '../../app/idam-api/IdamAPI';
-import axios, { AxiosInstance } from 'axios';
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 import { HTTPError } from '../../app/errors/HttpError';
 import { constants as http } from 'http2';
 import { Session } from 'express-openid-connect';
